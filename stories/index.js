20408966824df1c641ba370d447ee835cb2249b5
ItsJonQ <itsjonq@gmail.com>
2017-07-21 17:31:50 -0400

    WIP: Textarea

--- stories/index.js
+++ stories/index.js
@@ -6,10 +6,12 @@ import { linkTo } from '@storybook/addon-links';
 
 import '../src/styles/blue.scss';
 import {
-  Input
+  Input,
+  Textarea,
 } from '../src/index.js';
 
 storiesOf('Input', module)
+  .add('default', () => <Input />)
   .add('placeholder', () => <Input placeholder="Hello" autoFocus />)
   .add('prefix + suffix', () => <Input prefix="$" suffix=".00" autoFocus />)
   .add('seamless', () => <Input seamless autoFocus />)
@@ -18,3 +20,7 @@ storiesOf('Input', module)
   .add('success', () => <Input success="You're Awesome!" autoFocus />)
   .add('warning', () => <Input warning autoFocus />)
   .add('small', () => <Input size="sm" autoFocus />)
+
+storiesOf('Textarea', module)
+  .add('default', () => <Textarea />)
+  .add('placeholder', () => <Textarea placeholder="Hello" autoFocus />)
