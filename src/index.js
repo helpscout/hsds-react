d13f7dc8a0a72064d013aa34155f2ac3965a12f1
ItsJonQ <itsjonq@gmail.com>
2017-07-21 13:04:07 -0400

    Removing other create-react-app files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import './styles/blue.scss';
 export * from './components';
--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import './styles/blue.scss';
 import {
   Input
 } from '../src/index.js';
