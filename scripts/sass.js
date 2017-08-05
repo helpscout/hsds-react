ce0cce307374e05db1236dad83c5cabe7ce22919
ItsJonQ <itsjonq@gmail.com>
2017-08-05 13:22:19 -0400

    Add Sass build script

--- scripts/sass.js
+++ scripts/sass.js
@@ -1,28 +1,27 @@
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const harvester = require('seed-harvester');
-const sass = require('node-sass');
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const harvester = require('seed-harvester')
+const sass = require('node-sass')
 
 const includePaths = harvester([
   './src/styles'
-]);
+])
 
 // Default .css compile
 sass.render({
   file: './src/styles/blue.scss',
   includePaths: includePaths,
   outputStyle: 'compact'
-}, function(error, result) {
+}, function (error, result) {
   if (error) {
-    console.error(error);
-    return process.exit(1);
-  }
-  else {
-    mkdirp('./lib');
-    fs.writeFile('./lib/styles.css', result.css, function(err){
-      if(!err){
-        return console.log('styles.css created.');
+    console.error(error)
+    return process.exit(1)
+  } else {
+    mkdirp('./lib')
+    fs.writeFile('./lib/styles.css', result.css, function (err) {
+      if (!err) {
+        return console.log('styles.css created.')
       }
     })
   }
-});
+})
