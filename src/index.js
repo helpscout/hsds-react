0719b43643b85fa4c241a78b45c271005f89b98a
ItsJonQ <itsjonq@gmail.com>
2017-09-05 15:42:06 -0400

    Add utilities to main index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import { classNames, variantClassNames } from './utilities/classNames'
+import { createUniqueIDFactory } from './utilities/id'
+
 export * from './components'
+
+export const utilities = {
+  classNames,
+  createUniqueIDFactory,
+  variantClassNames
+}
