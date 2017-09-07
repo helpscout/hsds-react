2be214f06a4b32f4bda729d3c0c5b01f4478edc5
ItsJonQ <itsjonq@gmail.com>
2017-09-07 15:43:12 -0400

    Add ProgressBar
    This update adds a new ProgressBar component, complete with README and tests.
    
    Minor update: The className for the Animate component was renamed from `animate` to `Animate`.

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,70 @@
+import React, { PureComponent as Component } from 'react'
+import PropTypes from 'prop-types'
+import { classNames } from '../../utilities/classNames'
+import { noop } from '../../utilities/other'
+
+export const propTypes = {
+  onChange: PropTypes.func,
+  size: PropTypes.string,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ])
+}
+
+const defaultProps = {
+  onChange: noop,
+  value: 0
+}
+
+class ProgressBar extends Component {
+  componentWillReceiveProps (nextProps) {
+    const { onChange } = this.props
+    const value = this.getValueAsPercent(nextProps.value)
+    onChange(value)
+  }
+
+  getValue (val) {
+    const value = val !== null ? val : this.props.value
+    const barValue = parseFloat(value)
+    const normalizedBarValue =
+      barValue > 100 ? 100
+      : barValue < 0 ? 0
+      : barValue
+
+    return normalizedBarValue
+  }
+
+  getValueAsPercent (val = null) {
+    return `${this.getValue(val)}%`
+  }
+
+  render () {
+    const {
+      className,
+      size,
+      value,
+      ...rest
+    } = this.props
+
+    const componentClassName = classNames(
+      'c-ProgressBar',
+      size && `is-${size}`,
+      className
+    )
+    const progresBarStyle = {
+      width: this.getValueAsPercent()
+    }
+
+    return (
+      <div className={componentClassName} {...rest}>
+        <div className='c-ProgressBar__bar' style={progresBarStyle} />
+      </div>
+    )
+  }
+}
+
+ProgressBar.propTypes = propTypes
+ProgressBar.defaultProps = defaultProps
+
+export default ProgressBar
