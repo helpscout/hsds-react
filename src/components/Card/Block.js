81446a31a077f1d9a19e42220e2e234ef6b7c674
ItsJonQ <itsjonq@gmail.com>
2017-09-13 16:29:32 -0400

    Card: Enhance with flex and scrollable styles + features
    In this update, the `Card` and `Card.Block` components have been enhanced with the `flex` prop, which enable flexbox styling. `Card.Block` also receives a `scrollable` prop, which enables a more seamless integration with the `Scrollable` compnent.

--- src/components/Card/Block.js
+++ src/components/Card/Block.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from '../../utilities/classNames'
+import Scrollable from '../Scrollable'
 import { standardSizeTypes } from '../../constants/propTypes'
 
 export const propTypes = {
+  bgMuted: PropTypes.bool,
   className: PropTypes.string,
+  scrollable: PropTypes.bool,
+  flex: PropTypes.bool,
   size: standardSizeTypes
 }
 
 const Block = props => {
-  const { className, children, size, ...rest } = props
+  const {
+    bgMuted,
+    className,
+    children,
+    scrollable,
+    flex,
+    size,
+    ...rest
+  } = props
 
   const componentClassName = classNames(
     'c-card__block',
+    bgMuted && 'is-bg-muted',
+    flex && 'is-flex',
+    scrollable && 'is-scrollable',
     size && `c-card__block--${size}`,
     className
   )
 
-  return (
+  const scrollableClassName = classNames(
+    'c-card__block',
+    'c-card__block--scrollable',
+    flex && 'is-flex',
+    scrollable && 'is-scrollable'
+  )
+
+  const blockMarkup = scrollable ? (
+    <Scrollable className={scrollableClassName}>
+      <div className={componentClassName} {...rest}>
+        {children}
+      </div>
+    </Scrollable>
+  ) : (
     <div className={componentClassName} {...rest}>
       {children}
     </div>
   )
+
+  return blockMarkup
 }
 
 Block.propTypes = propTypes
