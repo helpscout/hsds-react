d44c14c99c3b07138364f4dd413b0a2c55354fc3
ItsJonQ <itsjonq@gmail.com>
2017-09-22 13:01:09 -0400

    Disabling storybook/addon-options
    This randomly started causing issues. Disappointing :(

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import '@storybook/addon-options/register'
-import '../stories/addons/prettyDocs'
+// import '../stories/addons/prettyDocs'
