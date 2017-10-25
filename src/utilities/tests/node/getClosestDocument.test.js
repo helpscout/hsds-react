805aa00c20d0293e49b40df0da24fb3b391bcc94
ItsJonQ <itsjonq@gmail.com>
2017-10-25 12:13:04 -0400

    Add tests

---
+++ src/utilities/tests/node/getClosestDocument.test.js
@@ -0,0 +1,33 @@
+import {
+  getClosestDocument
+} from '../../node'
+
+afterEach(() => {
+  global.document.innerHTML = ''
+})
+
+test('Returns global document, by default', () => {
+  expect(getClosestDocument() === document).toBeTruthy()
+})
+
+test('Returns global document, if argument is not a node', () => {
+  expect(getClosestDocument('div') === document).toBeTruthy()
+})
+
+test('Returns global document, node parent is global document', () => {
+  const o = document.createElement('div')
+  document.body.appendChild(o)
+
+  expect(getClosestDocument(o) === document).toBeTruthy()
+})
+
+test('Returns closest document of iframe node', () => {
+  const iframe = document.createElement('iframe')
+  document.body.appendChild(iframe)
+  iframe.contentDocument.write('<div></div>')
+
+  const o = iframe.contentDocument.querySelector('div')
+  const iframeDocument = iframe.contentDocument
+
+  expect(getClosestDocument(o) === iframeDocument).toBeTruthy()
+})
