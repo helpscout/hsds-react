96f828eb9cd675004fc9d863fff448d8b69c7e99
ItsJonQ <itsjonq@gmail.com>
2017-11-07 09:48:35 -0500

    Add Helix + simple integration tests
    This update adds Helix, our fixture generating library.
    Simple integration tests with Helix has also been added.

---
+++ src/tests/libraries/helix/createSpec.test.js
@@ -0,0 +1,31 @@
+import { createSpec, faker } from '@helpscout/helix'
+
+test('Can create and generate a spec', () => {
+  const Spec = createSpec({
+    id: faker.random.number(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName()
+  })
+
+  const fixture = Spec.generate()
+
+  expect(typeof fixture.id).toBe('number')
+  expect(typeof fixture.firstName).toBe('string')
+  expect(typeof fixture.lastName).toBe('string')
+})
+
+test('Can seed fixture results', () => {
+  const Spec = createSpec({
+    id: faker.random.number(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName()
+  })
+
+  const one = Spec.seed(1).generate()
+  const two = Spec.seed(2).generate()
+  const three = Spec.seed(1).generate()
+
+  expect(one.id).not.toBe(two.id)
+  expect(two.id).not.toBe(three.id)
+  expect(three.id).toBe(one.id)
+})
