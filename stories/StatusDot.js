0328327498eaa40071b3cd6def9686d657b98965
ItsJonQ <itsjonq@gmail.com>
2017-11-29 18:16:57 -0500

    StatusDot: Add additional props
    This update enhances the StatusDot component by providing it with
    additional props to have a more varied rendering style.
    
    Additional props include:
    
    * `inline` (ability to render the dot as `display: inline-block`)
    * `size` (`md`|`sm`)
    * `outerBorderColor`
    * a new `active` state (which renders a green dot)
    
    The new `active` state allows for this component to be more general,
    instead of using `online` as the state for a green color.
    
    Documentation and tests have been updated.

---
+++ stories/StatusDot.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { StatusDot } from '../src/index.js'
+
+const stories = storiesOf('StatusDot', module)
+
+stories.add('default', () => (
+  <StatusDot />
+))
+
+stories.add('borderColor', () => (
+  <div>
+    <div>
+      <StatusDot status='online' inline borderColor='red' />: Online
+    </div>
+  </div>
+))
+
+stories.add('outerBorderColor', () => (
+  <div>
+    <div>
+      <StatusDot status='online' inline outerBorderColor='red' />: Online
+    </div>
+  </div>
+))
+
+stories.add('states', () => (
+  <div>
+    <div>
+      <StatusDot status='online' inline />: Online
+    </div>
+    <div>
+      <StatusDot status='offline' inline />: Offline
+    </div>
+    <div>
+      <StatusDot status='busy' inline />: Busy
+    </div>
+    <div>
+      <StatusDot status='active' inline />: Active
+    </div>
+    <div>
+      <StatusDot status='inactive' inline />: Inactive
+    </div>
+  </div>
+))
+
+stories.add('sizes', () => (
+  <div>
+    <div>
+      <StatusDot inline size='md' />: MD
+    </div>
+    <div>
+      <StatusDot inline size='sm' />: SM
+    </div>
+  </div>
+))
