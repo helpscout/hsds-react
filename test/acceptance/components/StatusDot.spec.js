9c50e7c33b644463470dc286ed0b156383bcfd76
ItsJonQ <itsjonq@gmail.com>
2017-11-30 17:13:53 -0500

    Add acceptance test for StatusDot

---
+++ test/acceptance/components/StatusDot.spec.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StatusDot } from '../../../src/index'
+
+describe('Status', () => {
+  it('should not align adjacent to sibling node by default', () => {
+    mount(
+      <div>
+        <StatusDot className='dot' />
+        <span className='text'>Text</span>
+      </div>
+    )
+    const dot = $('.dot')
+    const text = $('.text')
+
+    expect(text.offset().left).toBe(dot.offset().left)
+    expect(text.offset().top).toBeGreaterThan(dot.offset().top + dot.height())
+  })
+
+  it('should align adjacent to sibling node, if inline', () => {
+    mount(
+      <div>
+        <StatusDot inline className='dot' />
+        <span className='text'>Text</span>
+      </div>
+    )
+    const dot = $('.dot')
+    const text = $('.text')
+
+    expect(text.offset().left).toBeGreaterThan(dot.offset().left)
+    expect(text.offset().top).toBeLessThan(dot.offset().top + dot.height())
+  })
+})
