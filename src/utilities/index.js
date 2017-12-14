f0cdc46112a1198c1bdef339f9adbcbbb38009f1
ItsJonQ <itsjonq@gmail.com>
2017-12-14 17:49:57 -0500

    Utilities: Expose all of Blue's utils
    This update exposes all of Blue's utils, making them available to be
    imported and used.
    
    All of the utilities come bundled within a `utilities` object.
    
    **Example**
    
    ```js
    import { utilities } from '@helpscout/blue`
    
    const {
      smoothScroll
    } = utilities
    
    // Now you can do things with smoothScroll.smoothScrollTo()!
    ```

---
+++ src/utilities/index.js
@@ -0,0 +1,31 @@
+import * as animation from './animation'
+import * as classNames from './classNames'
+import * as component from './component'
+import * as easing from './easing'
+import * as focus from './focus'
+import * as globalManager from './globalManager'
+import * as id from './id'
+import * as node from './node'
+import * as nodePosition from './nodePosition'
+import * as other from './other'
+import * as scrollFade from './scrollFade'
+import * as smoothScroll from './smoothScroll'
+import * as strings from './strings'
+import * as types from './types'
+
+export default {
+  animation,
+  classNames,
+  component,
+  easing,
+  focus,
+  globalManager,
+  id,
+  node,
+  nodePosition,
+  other,
+  scrollFade,
+  smoothScroll,
+  strings,
+  types
+}
