06e8a00f1260bad05fc9284afc35e7d3c89e81bd
Tom Graham <tompedals@users.noreply.github.com>
2018-04-23 17:44:15 +0100

    Update story to show when the timestamp is updated

--- stories/Timestamp.js
+++ stories/Timestamp.js
@@ -4,16 +4,21 @@ import { Timestamp } from '../src/index.js'
 
 const stories = storiesOf('Timestamp', module)
 
-const customFormatter = timestamp => `Yes!: ${timestamp}`
+let count = 0
+const customFormatter = timestamp => {
+  const now = (new Date()).toISOString()
+
+  return `${timestamp} (update ${count++} at ${now})`
+}
 
 stories.add('default', () => (
   <Timestamp timestamp='10:41am' />
 ))
 
 stories.add('live', () => (
-  <Timestamp timestamp='10:41am' live />
+  <Timestamp timestamp={(new Date()).toISOString()} live />
 ))
 
 stories.add('formatter', () => (
-  <Timestamp timestamp='10:41am' live formatter={customFormatter} />
+  <Timestamp timestamp={(new Date()).toISOString()} live formatter={customFormatter} />
 ))
