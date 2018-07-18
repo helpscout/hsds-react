54989aa9a74fa8943d2cdbf91748f4369c462f27
tjbo <tomoconnor@helpscout.com>
2018-07-18 16:22:28 +0200

    Change refApplySubmit to refApplyCallStopTyping

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -49,7 +49,7 @@ type Props = {
   placeholder: string,
   prefix: string,
   readOnly: boolean,
-  refApplySubmit: (event: SubmitEvent) => void,
+  refApplyCallStopTyping: (event: SubmitEvent) => void,
   removeStateStylesOnFocus: boolean,
   resizable: boolean,
   seamless: boolean,
@@ -88,7 +88,7 @@ class Input extends Component<Props, State> {
     onStopTyping: noop,
     onWheel: noop,
     readOnly: false,
-    refApplySubmit: noop,
+    refApplyCallStopTyping: noop,
     removeStateStylesOnFocus: false,
     resizable: false,
     scrollLock: false,
@@ -118,7 +118,7 @@ class Input extends Component<Props, State> {
   componentDidMount() {
     this.maybeForceAutoFocus()
     this.props.withTypingEvent &&
-      this.props.refApplySubmit(this.callStopTyping.bind(this))
+      this.props.refApplyCallStopTyping(this.callStopTyping.bind(this))
   }
 
   componentWillReceiveProps(nextProps: Props) {
@@ -299,7 +299,7 @@ class Input extends Component<Props, State> {
       placeholder,
       prefix,
       readOnly,
-      refApplySubmit,
+      refApplyCallStopTyping,
       removeStateStylesOnFocus,
       resizable,
       seamless,
