c71b8338c0038375db0971460eb37be4ee1747ef
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-08-10 17:19:40 -0400

    Add tests

---
+++ src/components/Message/__tests__/Embed.test.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Embed from '../Embed'
+import Message from '../Message'
+
+const cx = 'c-MessageEmbed'
+
+const html = '<div><iframe src="https://example.com"></iframe></div>'
+
+describe('ClassNames', () => {
+  test('Has default className', () => {
+    const wrapper = mount(<Embed />)
+    const o = wrapper.find(`.${cx}`)
+    expect(o.length).toBeTruthy()
+  })
+
+  test('Accepts custom classNames', () => {
+    const wrapper = mount(<Embed className="mugatu" />)
+    const o = wrapper.find(`.${cx}`)
+    expect(o.hasClass('mugatu')).toBeTruthy()
+  })
+})
+
+describe('Context', () => {
+  test('Adds className based on context.theme', () => {
+    const wrapper = mount(
+      <Message.Provider theme="embed">
+        <Embed />
+      </Message.Provider>
+    )
+    const el = wrapper.find(`.${cx}`)
+    expect(el.props().className).toContain('is-theme-embed')
+  })
+})
+
+describe('Content', () => {
+  test('Renders html inside component', () => {
+    const wrapper = mount(<Embed html={html} />)
+    const o = wrapper.find(`.${cx}`)
+    expect(o.html()).toContain(html)
+  })
+})
+
+describe('Display name', () => {
+  test('Has a display name', () => {
+    expect(Embed.displayName).toEqual('Styled(Message.Embed)')
+  })
+})
