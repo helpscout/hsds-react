43af15c0cc110f088382934a027ae81c7592f7ec
ItsJonQ <itsjonq@gmail.com>
2018-09-12 16:59:25 -0400

    Adjust React SFC -> component classes

--- src/components/KeypressListener/index.js
+++ src/components/KeypressListener/index.js
@@ -1,7 +1,7 @@
 // Modified version of:
 // https://github.com/Shopify/polaris/blob/master/src/components/KeypressListener/KeypressListener.tsx
 
-import React, { PureComponent as Component } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { addEventListener, removeEventListener } from '../../utilities/events'
