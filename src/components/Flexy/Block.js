c71d58e0ab2dd79104d7a6a0edde48b655e8ee55
Jon Quach <hello@jonquach.com>
2018-09-19 16:05:54 -0400

    Flexy: Refactor with CSS-in-JS (#351)
    * Flexy: Refactor with CSS-in-JS
    
    This update converts the `Flexy` component's CSS into CSS-in-JS styling,
    via the `@helpscout/fancy` library.
    
    Additional Stories were added for `Flexy` to test out the various
    configurationsm such as `align`, `gap`, and `just`.
    
    `Flexy` has also been modernized with the latest Blue component standards,
    which include namespacing and `propConnect`.
    
    Lastly, the tests were updated to use `mount` instead of `shallow` for better
    reliability.
    
    Bonus lastly: Since the CSS will be compiled using CSS-in-JS, this allowed me
    to remove `seed-flexy` as a dependency :raised_hands:
    
    * Update Flexy with getValidProps around rest spread
    
    * Update package-lock.json [skip ci]
    
    * 1.4.2-0

--- src/components/Flexy/Block.js
+++ src/components/Flexy/Block.js
@@ -1,6 +1,10 @@
 // @flow
 import React, { PureComponent as Component } from 'react'
-import classNames from '../../utilities/classNames'
+import getValidProps from '@helpscout/react-utils/dist/getValidProps'
+import { classNames } from '../../utilities/classNames'
+import { namespaceComponent } from '../../utilities/component'
+import { COMPONENT_KEY } from './utils'
+import { BlockUI } from './styles/Block.css.js'
 
 type Props = {
   children?: any,
@@ -8,19 +12,19 @@ type Props = {
 }
 
 class Block extends Component<Props> {
-  static displayName = 'Flexy.Block'
-
   render() {
     const { children, className, ...rest } = this.props
 
     const componentClassName = classNames('c-Flexy__block', className)
 
     return (
-      <div className={componentClassName} {...rest}>
+      <BlockUI {...getValidProps(rest)} className={componentClassName}>
         {children}
-      </div>
+      </BlockUI>
     )
   }
 }
+
+namespaceComponent(COMPONENT_KEY)(Block)
 
 export default Block
---
+++ src/components/Flexy/utils.js
@@ -0,0 +1,5 @@
+export const COMPONENT_KEY = {
+  Flexy: 'Flexy',
+  Block: 'Flexy.Block',
+  Item: 'Flexy.Item',
+}
