e440101a70e103bdc578e9cd0b29917a18415b2a
Jon Quach <hello@jonquach.com>
2018-09-21 08:24:18 -0400

    Emoticon: Add hover colors (#354)
    This update enhances the `Emoticon` component to change it's color
    on a `:hover` interaction. The component has also been refactored to
    use the latest Blue conventions, including `styled()`, `propConnect`,
    namespacing, and `mount` based testing.

---
+++ src/components/Emoticon/Emoticon.js
@@ -0,0 +1,80 @@
+// @flow
+import type { EmotionSize } from './types'
+import React, { PureComponent as Component } from 'react'
+import getValidProps from '@helpscout/react-utils/dist/getValidProps'
+import EMOTICONS from './emoticons'
+import { BEM, classNames } from '../../utilities/classNames'
+import { namespaceComponent } from '../../utilities/component'
+import { EmoticonUI } from './styles/Emoticon.css.js'
+import { COMPONENT_KEY } from './utils'
+
+type Props = {
+  className?: string,
+  center: boolean,
+  clickable: boolean,
+  isActive: boolean,
+  isDisabled: boolean,
+  inline: boolean,
+  name: string,
+  title?: string,
+  size?: EmotionSize,
+}
+
+class Emoticon extends Component<Props> {
+  static defaultProps = {
+    center: false,
+    clickable: true,
+    inline: false,
+    isActive: true,
+    isDisabled: false,
+    name: 'happy',
+    size: 'md',
+    title: '',
+  }
+
+  render() {
+    const {
+      className,
+      center,
+      clickable,
+      isActive,
+      isDisabled,
+      inline,
+      name,
+      title,
+      size,
+      ...rest
+    } = this.props
+
+    const src = { __html: EMOTICONS[name] }
+    const componentClassName = classNames(
+      'c-Emoticon',
+      !clickable && 'is-noInteract',
+      center && 'is-center',
+      inline && 'is-inline',
+      isActive && 'is-active',
+      isDisabled && 'is-disabled',
+      size && `is-${size}`,
+      className
+    )
+
+    const iconClassName = classNames(
+      BEM(componentClassName).element('icon'),
+      'c-Emoticon__icon'
+    )
+
+    return (
+      <EmoticonUI {...getValidProps(rest)} className={componentClassName}>
+        <span
+          className={iconClassName}
+          dangerouslySetInnerHTML={src}
+          title={title}
+        />
+      </EmoticonUI>
+    )
+  }
+}
+
+namespaceComponent(COMPONENT_KEY)(Emoticon)
+
+export default Emoticon
