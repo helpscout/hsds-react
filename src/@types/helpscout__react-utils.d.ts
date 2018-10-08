d8c078a3efca90fabf94d3e34c479f0e67d0efb6
Jon Q <itsjonq@gmail.com>
2018-10-08 13:59:50 -0400

    Improve type support for external libraries

--- src/@types/helpscout__react-utils.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'getValidProps' {
-  function getValidProps(): any
-  export = getValidProps
-}
---
+++ src/@types/helpscout__react-utils/index.d.ts
@@ -0,0 +1,34 @@
+type Component = any
+type WrappedComponent = any
+type Context = {
+  Provider: Component
+  Consumer: Component
+}
+
+declare module '@helpscout/react-utils/dist/classNames' {
+  function classNames(...classes: any): string
+  export = classNames
+}
+
+declare module '@helpscout/react-utils/dist/createContext' {
+  function createContext(value: any): Context
+  export = createContext
+}
+
+declare module '@helpscout/react-utils/dist/getComponentName' {
+  function getComponentName(component: Component): string
+  export = getComponentName
+}
+
+declare module '@helpscout/react-utils/dist/getValidProps' {
+  function getValidProps(props: Object): Object
+  export = getValidProps
+}
+
+declare module '@helpscout/react-utils/dist/hoistNonReactStatics' {
+  function hoistNonReactStatics(
+    WrappedComponent: WrappedComponent,
+    component: Component
+  ): WrappedComponent
+  export = hoistNonReactStatics
+}
