462832dc84310e62b8806006a41c89deeef2b88e
Jon Q <itsjonq@gmail.com>
2018-10-10 18:52:13 -0400

    FormGroup: Add responsive support + improve horizontal alignment styles

--- src/components/FormGroup/styles/Choice.css.js
+++ src/components/FormGroup/styles/Choice.css.js
@@ -4,6 +4,12 @@ import styled from '../../styled'
 
 export const FormGroupChoiceUI = styled('div')`
   ${baseStyles} margin-bottom: 8px;
+
+  &.is-responsive {
+    flex: 1;
+    max-width: 100%;
+    min-width: 0;
+  }
 `
 
 export default FormGroupChoiceUI
