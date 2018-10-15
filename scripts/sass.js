ba0ce29e243072486f3bbe054de47ffa74ab473b
Jon Quach <hello@jonquach.com>
2018-10-15 16:22:27 -0400

    Seed: Update dependencies (#379)
    This update modifies the seed dependencies to use the new monorepo
    hosted seed packs.
    
    All that was required was to change the dependency from, say, `seed-this`,
    to `@seedcss/seed-this`, as well as to use the current latest version from
    `@seedcss`.
    
    The `@import` and `node-sass` build process can remain untouched, as
    `seed-harvester` has native support for the new `@seedcss` paths, including
    the legacy pack paths.

--- scripts/sass.js
+++ scripts/sass.js
@@ -3,47 +3,47 @@ const mkdirp = require('mkdirp')
 const harvester = require('seed-harvester')
 const sass = require('node-sass')
 
-const includePaths = harvester([
-  './src/styles'
-])
+const includePaths = harvester(['./src/styles'])
 
 mkdirp('./dist')
 mkdirp('./dist/css')
 mkdirp('./dist/scss')
 
-const files = [
-  'blue',
-  'blue.hs-app'
-]
+const files = ['blue', 'blue.hs-app']
 
-const renderFile = (fileName) => {
+const renderFile = fileName => {
   // Default .css compile
-  return sass.render({
-    file: `./src/styles/${fileName}.scss`,
-    includePaths: includePaths,
-    outputStyle: 'compact'
-  }, function (error, result) {
-    if (error) {
-      console.error(error)
-      return process.exit(1)
-    } else {
-      fs.writeFile(`./dist/css/${fileName}.css`, result.css, function (err) {
-        if (err) {
-          console.error(error)
-          return process.exit(1)
-        }
-        console.log(`${fileName}.css created.`)
-
-        fs.writeFile(`./dist/scss/${fileName}.scss`, result.css, function (err) {
+  return sass.render(
+    {
+      file: `./src/styles/${fileName}.scss`,
+      includePaths: includePaths,
+      outputStyle: 'compact',
+    },
+    function(error, result) {
+      if (error) {
+        console.error(error)
+        return process.exit(1)
+      } else {
+        fs.writeFile(`./dist/css/${fileName}.css`, result.css, function(err) {
           if (err) {
             console.error(error)
             return process.exit(1)
           }
-          console.log(`${fileName}.scss created.`)
+          console.log(`${fileName}.css created.`)
+
+          fs.writeFile(`./dist/scss/${fileName}.scss`, result.css, function(
+            err
+          ) {
+            if (err) {
+              console.error(error)
+              return process.exit(1)
+            }
+            console.log(`${fileName}.scss created.`)
+          })
         })
-      })
+      }
     }
-  })
+  )
 }
 
 files.forEach(file => renderFile(file))
