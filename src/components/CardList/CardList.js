ba5b517229d17f655de1a240caf90e542b16e6ac
Tom Graham <tompedals@users.noreply.github.com>
2018-10-17 15:52:49 +0100

    Card list component (#382)
    * Add CardList component to wrap Cards with animation
    
    * Add components to the README
    
    * Add test for CardList
    
    And attempt to optimize <Child key={} /> when mapping over CardList
    children.

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,65 @@
+// @flow
+import React, { PureComponent } from 'react'
+import Animate from '../Animate'
+import AnimateGroup from '../AnimateGroup'
+import {
+  isComponentTypeCard,
+  namespaceComponent,
+} from '../../utilities/component'
+import { COMPONENT_KEY } from './utils'
+import classNames from '../../utilities/classNames'
+
+type Props = {
+  animationEasing: string,
+  animationSequence: string,
+  animationStagger: number,
+  children?: any,
+  className?: string,
+  stagger: boolean,
+}
+
+class CardList extends PureComponent<Props> {
+  static defaultProps = {
+    animationEasing: 'ease',
+    animationSequence: 'fade up',
+    animationStagger: 60,
+    stagger: true,
+  }
+
+  getChildrenMarkup = () => {
+    const { animationEasing, animationSequence, children } = this.props
+
+    return React.Children.map(children, (child, index) => {
+      if (!isComponentTypeCard(child)) {
+        return null
+      }
+      const id = child.props.id || child.key || `cardListChild-${index}`
+
+      return (
+        <Animate key={id} easing={animationEasing} sequence={animationSequence}>
+          {child}
+        </Animate>
+      )
+    })
+  }
+
+  render() {
+    const { animationStagger, className, ...rest } = this.props
+    const componentClassName = classNames('c-CardList', className)
+
+    return (
+      <AnimateGroup
+        {...rest}
+        className={componentClassName}
+        stagger
+        staggerDelay={animationStagger}
+      >
+        {this.getChildrenMarkup()}
+      </AnimateGroup>
+    )
+  }
+}
+
+namespaceComponent(COMPONENT_KEY)(CardList)
+
+export default CardList
