dd1ce7848618eb7857e3983ce247ffecb18847ec
ItsJonQ <itsjonq@gmail.com>
2018-11-09 18:43:14 -0500

    Fix StatusBar tests

--- src/components/StatusBar/tests/StatusBar.test.js
+++ src/components/StatusBar/tests/StatusBar.test.js
@@ -75,12 +75,15 @@ describe('Collapsible', () => {
 
   test('Should pass isOpen prop to Collapsible', () => {
     const wrapper = mount(<StatusBar />)
-    const o = wrapper.find(Collapsible)
 
     wrapper.setProps({ isOpen: true })
+
+    let o = wrapper.find(Collapsible)
     expect(o.props().isOpen).toBeTruthy()
 
     wrapper.setProps({ isOpen: false })
+
+    o = wrapper.find(Collapsible)
     expect(o.props().isOpen).not.toBeTruthy()
   })
 
