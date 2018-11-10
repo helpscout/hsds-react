2c19efe17d1fd6fadb012fa6ee6476f6b29bb4d2
ItsJonQ <itsjonq@gmail.com>
2018-11-10 14:06:29 -0500

    Upgrade to Babel 7!!
    Tested Storybook, build, and Jest tests

--- src/components/Huzzah/__tests__/getHuzzah.test.js
+++ src/components/Huzzah/__tests__/getHuzzah.test.js
@@ -1,25 +1,26 @@
-import { getHuzzah, DEFAULT_HUZZAH } from '../index'
+import { getHuzzah } from '../index'
+import HUZZAHS from '../huzzahs'
 
 describe('getHuzzah', () => {
   test('Provides a default fallback huzzah', () => {
-    expect(getHuzzah()).toContain(DEFAULT_HUZZAH)
+    expect(getHuzzah()).toContain(HUZZAHS.donut)
   })
 
   test('Provides a default fallback huzzah, if name is not defined', () => {
-    expect(getHuzzah('wut-no-lol')).toContain(DEFAULT_HUZZAH)
+    expect(getHuzzah('wut-no-lol')).toContain(HUZZAHS.donut)
   })
 
   test('Retrieves huzzah, if defined', () => {
-    expect(getHuzzah('rocket')).toContain('rocket')
-    expect(getHuzzah('rocket')).not.toContain(DEFAULT_HUZZAH)
+    expect(getHuzzah('rocket')).toContain(HUZZAHS.rocket)
+    expect(getHuzzah('rocket')).not.toContain(HUZZAHS.donut)
   })
 
   test('Retrieves huzzah with hyphen', () => {
-    expect(getHuzzah('mic-drop')).toContain('mic-drop')
-    expect(getHuzzah('mic-drop')).not.toContain(DEFAULT_HUZZAH)
+    expect(getHuzzah('mic-drop')).toContain(HUZZAHS['mic-drop'])
+    expect(getHuzzah('mic-drop')).not.toContain(HUZZAHS.donut)
   })
 
   test('Handles uppercase letters', () => {
-    expect(getHuzzah('RoCKEt')).toContain('rocket')
+    expect(getHuzzah('RoCKEt')).toContain(HUZZAHS.rocket)
   })
 })
