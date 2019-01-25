559fb46c18a5f1d1414e252a80db20a5e787e754
tjbo <tomoconnor@helpscout.com>
2019-01-25 15:42:00 -0800

    add-carriage-returns-for-alt-keys

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -115,6 +115,7 @@ export class Input extends Component<Props, State> {
     forceAutoFocusTimeout: 0,
     inputRef: noop,
     innerRef: noop,
+    hasInsertCarriageReturns: false,
     isFocused: false,
     isFirst: false,
     isNotOnly: false,
@@ -350,7 +351,30 @@ export class Input extends Component<Props, State> {
     onWheel(event)
   }
 
+  insertCarriageReturnAtCursorIndex(event) {
+    /* istanbul ignore next */
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) return
+    event.stopPropagation()
+    const cursorIndex = event.currentTarget.selectionStart
+    const currentValue = event.currentTarget.value
+    event.currentTarget.value = `${currentValue.substr(
+      0,
+      cursorIndex
+    )}\n${currentValue.substr(cursorIndex)}`
+    // next tick hack for caret insertion
+    setTimeout(() => {
+      this.inputNode.setSelectionRange(cursorIndex + 1, cursorIndex + 1)
+      console.log(this.inputNode)
+    }, 1)
+  }
+
   handleOnKeyDown = (event: Event) => {
+    const { hasInsertCarriageReturns } = this.props
+
+    if (hasInsertCarriageReturns && event.keyCode === 13) {
+      this.insertCarriageReturnAtCursorIndex(event)
+    }
+
     this.props.onKeyDown(event)
     this.scrollToBottom()
   }
