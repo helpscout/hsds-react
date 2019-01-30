9e720e531b3c925a9608508a3dfd8ebf3b2643e8
tjbo <tomoconnor@helpscout.com>
2019-01-30 11:58:50 -0800

    Add utility to test for modifier key press

---
+++ src/utilities/__tests__/keys.test.ts
@@ -0,0 +1,39 @@
+import { isModifierKeyPressed } from '../keys'
+
+describe('isArray', () => {
+  test('return true if ctrl is pressed', () => {
+    const event = {
+      altKey: false,
+      ctrlKey: true,
+      metaKey: false,
+    }
+    expect(isModifierKeyPressed(event)).toBe(true)
+  })
+
+  test('return true if altKey is pressed', () => {
+    const event = {
+      altKey: true,
+      ctrlKey: false,
+      metaKey: false,
+    }
+    expect(isModifierKeyPressed(event)).toBe(true)
+  })
+
+  test('return true if altKey is pressed', () => {
+    const event = {
+      altKey: false,
+      ctrlKey: false,
+      metaKey: true,
+    }
+    expect(isModifierKeyPressed(event)).toBe(true)
+  })
+
+  test('return false if all modifier keys are false', () => {
+    const event = {
+      altKey: false,
+      ctrlKey: false,
+      metaKey: false,
+    }
+    expect(isModifierKeyPressed(event)).toBe(false)
+  })
+})
