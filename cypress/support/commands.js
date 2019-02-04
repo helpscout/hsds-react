bde8a9c3cd253c2f24c56dcf4f3aaac9ed3989f2
Jon Q <hello@jonquach.com>
2019-02-04 10:39:34 -0500

    Re-Add cypress enhancements

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('visitStory', (kind = '', name = '') => {
+  const url = getStoryBookUrl(kind, name)
+
+  return cy.visit(url)
+})
+
+/**
+ * Generates the iFrame URL for a given component/story.
+ * @param {string} kind The Component
+ * @param {string} name The name of the Story
+ */
+function getStoryBookUrl(kind = 'ArticleCard', name = 'Content') {
+  return `iframe.html?selectedKind=${kind}%2FV2&selectedStory=${name}`
+}
