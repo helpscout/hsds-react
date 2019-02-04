be80b68870232727c8f97440555d89f30141469c
Jon Q <hello@jonquach.com>
2019-02-04 09:45:11 -0500

    Improve Cypress + lock dependencies to specified versions

--- cypress/integration/Button.js
+++ cypress/integration/Button.js
@@ -1,6 +1,6 @@
 describe('Button', function() {
   it('Can click a Button', function() {
-    cy.visit('iframe.html?selectedKind=Button%2FV2&selectedStory=everything')
+    cy.visitStory('Button', 'everything')
 
     cy.get('[data-cy="Button"]:first').click()
   })
