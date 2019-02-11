188332d42838a3019c19a11d3e4f3c8f3460c7ff
Jon Quach <hello@jonquach.com>
2019-02-11 12:04:35 -0500

    Storybook: Enhance with addons (#478)
    This update increments the Storybook packages to the latest versions. It
    also adds `@helpscout/stats`, and the knobs and Viewport addons globally.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,31 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { addDecorator, configure, addParameters } from '@storybook/react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { withOptions } from '@storybook/addon-options'
+import { withKnobs } from '@storybook/addon-knobs'
+import { StatsGraph } from '@helpscout/stats'
 import '../src/styles/blue.scss'
+
+const withStats = storyFn => (
+  <div>
+    <StatsGraph bottom={0} top="initial" right={5} />
+    {storyFn()}
+  </div>
+)
+
+addDecorator(withStats)
+addDecorator(withKnobs)
+addDecorator(
+  withOptions({
+    name: 'HSDS: React',
+  })
+)
+
+addParameters({
+  viewports: {
+    ...INITIAL_VIEWPORTS,
+  },
+})
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.(js|ts|tsx)$/)
