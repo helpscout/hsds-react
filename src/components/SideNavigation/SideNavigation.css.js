48a0d0d8dcfcc6fbf559388c26f5d95a65a62972
Pierre-Luc Babin <plbabin@gmail.com>
2019-02-18 19:59:31 +0000

    Add Sidebar story wrapper. Add base styling for SideNavigation

--- src/components/SideNavigation/SideNavigation.css.js
+++ src/components/SideNavigation/SideNavigation.css.js
@@ -4,6 +4,14 @@ import { getColor } from '../../styles/utilities/color'
 
 export const SideNavigationUI = styled('div')`
   ${baseStyles};
+  background-color: ${getColor('grey.300')};
+  border-right: 1px solid ${getColor('grey.500')};
+  height: 100%;
+  width: 250px;
+
+  &.is-collapsed {
+    width: 50px;
+  }
 `
 
 export const SideNavigationSectionUI = styled('div')``
--- stories/SideNavigation.stories.js
+++ stories/SideNavigation.stories.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { SideNavigation } from '../src/index.js'
+import { SideNavigation, Flexy, Heading } from '../src/index.js'
 import styled from '../src/components/styled'
+import { render } from 'enzyme'
+
+class SidebarWrapper extends React.PureComponent {
+  render() {
+    return (
+      <Flexy style={{ height: '90vh' }} gap="xl" align="top">
+        <Flexy.Item
+          style={{
+            height: '100%',
+          }}
+        >
+          {this.props.children}
+        </Flexy.Item>
+        <Flexy.Block>
+          <Heading>CONTENT</Heading>
+        </Flexy.Block>
+      </Flexy>
+    )
+  }
+}
 
 const stories = storiesOf('SideNavigation', module)
+stories.addDecorator(storyFn => <SidebarWrapper>{storyFn()}</SidebarWrapper>)
 
 stories.add('default', () => <SideNavigation />)
