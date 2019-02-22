98b08e5166f2dd2efce615f24d12f56440aaea88
Jon Q <hello@jonquach.com>
2019-02-22 10:31:57 -0500

    Refactor and improve RouteWrapper

--- src/components/RouteWrapper/index.js
+++ src/components/RouteWrapper/index.js
@@ -2,67 +2,63 @@ import React from 'react'
 import hoistNonReactStatics from '@helpscout/react-utils/dist/hoistNonReactStatics'
 import PropTypes from 'prop-types'
 import { isDefined } from '../../utilities/is'
+import { noop } from '../../utilities/other'
+import get from '../../utilities/get'
 
-/**
- * Adds `fetch` and `to` attributes to a Component, allowing clicks to route,
- * optionally fetching data asynchronously first
- *
- * @param WrappedComponent
- * @return function
- * @constructor
- */
 const RouteWrapper = WrappedComponent => {
-  class Component extends React.PureComponent {
-    render() {
-      const {
-        fetch = () => Promise.resolve(),
-        to,
-        'data-bypass': dataByPass,
-        ...rest
-      } = this.props
-
-      if (to) {
-        if (this.context && this.context.router) {
-          const history = this.context.router.history
-          rest.onClick = e => {
-            if (e && (e.metaKey || e.ctrlKey)) {
-              // Allow ctrl+clicks to function normally
-              return
-            }
-            e && e.preventDefault()
-            fetch().then(() => {
-              history.push(to)
-            })
-          }
-        } else {
-          console.error(
-            'The `to` attribute can only be used in the the context of a React Router.'
-          )
-        }
-      }
-
-      const dataByPassValue = isDefined(dataByPass) ? dataByPass : !!to
-
-      return <WrappedComponent {...rest} data-bypass={dataByPassValue} />
-    }
-  }
-
-  Component.contextTypes = {
-    router: PropTypes.object,
-  }
-
-  Component.propTypes = {
-    fetch: PropTypes.func,
-    start: PropTypes.func,
-    to: PropTypes.string,
-  }
-
   const componentName =
     WrappedComponent.displayName ||
     WrappedComponent.name ||
     /* istanbul ignore next */
     'Component'
-  Component.displayName = `withRoute(${componentName})`
+
+  class Component extends React.PureComponent {
+    static contextTypes = {
+      router: PropTypes.object,
+    }
+
+    static defaultProps = {
+      onClick: noop,
+      fetch: () => Promise.resolve(),
+    }
+
+    static displayName = `withRoute(${componentName})`
+
+    handleOnClick = event => {
+      const { fetch, to } = this.props
+      const history = get(this, 'context.router.history')
+
+      this.props.onClick(event)
+
+      if (!to || !history) return
+
+      if (event && (event.metaKey || event.ctrlKey)) {
+        // Allow ctrl + clicks to function normally
+        return
+      }
+
+      event && event.preventDefault()
+
+      fetch()
+        .then(() => history.push(to))
+        .catch(console.log)
+    }
+
+    render() {
+      const { fetch, href, to, 'data-bypass': dataByPass, ...rest } = this.props
+
+      const dataByPassValue = isDefined(dataByPass) ? dataByPass : !!to
+
+      return (
+        <WrappedComponent
+          {...rest}
+          href={href || to}
+          data-bypass={dataByPassValue}
+          onClick={this.handleOnClick}
+        />
+      )
+    }
+  }
 
   return hoistNonReactStatics(Component, WrappedComponent)
 }
