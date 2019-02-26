69463cc35f83908050685ce71832ebd102645246
Jon Q <hello@jonquach.com>
2019-02-26 17:16:52 -0500

    Adding Toolbar example

--- stories/Nav.stories.js
+++ stories/Nav.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Nav from '../src/components/Nav'
+import Toolbar from '../src/components/Toolbar'
 import { MemoryRouter as Router, Route } from 'react-router-dom'
 import {
   withKnobs,
@@ -13,15 +14,16 @@ import { withArtboard } from '@helpscout/artboard'
 
 const stories = storiesOf('Nav', module)
 
-stories.addDecorator(
-  withArtboard({
-    width: 500,
-    height: 200,
-    withCenterGuides: false,
-    showInterface: false,
-  })
-)
-stories.addDecorator(withKnobs)
+// stories.addDecorator(
+//   withArtboard({
+//     width: 600,
+//     height: 200,
+//     withCenterGuides: false,
+//     withResponsiveWidth: true,
+//     showInterface: false,
+//   })
+// )
+// stories.addDecorator(withKnobs)
 
 const RouteComponent = () => <div />
 
@@ -44,5 +46,27 @@ stories.add('Default', () => (
       <Route exact path="/two" component={RouteComponent} />
       <Route exact path="/three" component={RouteComponent} />
     </div>
+  </Router>
+))
+
+stories.add('Toolbar', () => (
+  <Router>
+    <Toolbar>
+      <Nav>
+        <Nav.Item to="/">Home</Nav.Item>
+        <Nav.Item to="/one">One</Nav.Item>
+        <Nav.Item to="/two">Two</Nav.Item>
+        <Nav.Item to="/three" error="Something went wrong">
+          Three
+        </Nav.Item>
+        <Nav.Item to="/four" disabled>
+          Four
+        </Nav.Item>
+      </Nav>
+      <Route exact path="/" component={RouteComponent} />
+      <Route exact path="/one" component={RouteComponent} />
+      <Route exact path="/two" component={RouteComponent} />
+      <Route exact path="/three" component={RouteComponent} />
+    </Toolbar>
   </Router>
 ))
