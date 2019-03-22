7967c16b68b0849b108c9ed3c23c5f58d9958abb
Jon Quach <hello@jonquach.com>
2019-03-22 10:58:00 -0400

    Link: Refactor + add voidOnClick (#534)
    * Link: Refactor + add voidOnClick
    
    This update refactors the `Link` component to TypeScript and use `css.ts`.
    The previous SCSS styles have been removed.
    
    `Link` has also been enhanced with a new prop, `voidOnClick`. This disabled
    the click interaction if set to `true`.
    
    * Remove test for link selector check

--- stories/Link.stories.js
+++ stories/Link.stories.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withArtboard } from '@helpscout/artboard'
+import { boolean, text } from '@storybook/addon-knobs'
 import { Link } from '../src/index.js'
 
-storiesOf('Link', module).add('default', () => (
-  <Link href="https://github.com/helpscout/hsds-react">Linky</Link>
-))
+const stories = storiesOf('Link', module)
+
+stories.addDecorator(
+  withArtboard({ width: 300, height: 100, withCenterGuides: false })
+)
+
+stories.add('Default', () => {
+  const props = {
+    children: text('Content', 'Linky'),
+    href: text('href', 'https://github.com/helpscout/hsds-react'),
+    voidOnClick: boolean('voidOnClick', false),
+  }
+
+  return <Link {...props} />
+})
