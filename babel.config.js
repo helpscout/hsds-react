afa03bf89a8f3e7d55c52a595b8c4567e08fe0a3
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2019-04-24 17:05:53 +0200

    improves babel conf

--- babel.config.js
+++ babel.config.js
@@ -1,55 +1,35 @@
-module.exports =
-  process.env.NODE_ENV === 'test'
-    ? {
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: {
-                ie: '11',
-                edge: '16',
-                firefox: '60',
-                chrome: '65',
-                safari: '10',
-                node: '8',
-              },
-              loose: true,
-            },
-          ],
-          '@babel/preset-flow',
-          '@babel/react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-transform-flow-strip-types',
-          '@babel/plugin-transform-runtime',
-          'inline-svg',
-          'emotion',
-        ],
-      }
-    : {
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: {
-                ie: '11',
-                edge: '16',
-                firefox: '60',
-                chrome: '65',
-                safari: '10',
-                node: '8',
-              },
-              loose: true,
-            },
-          ],
-          '@babel/preset-flow',
-          '@babel/react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-transform-flow-strip-types',
-          'inline-svg',
-          'emotion',
-        ],
-      }
+module.exports = api => {
+  const isTest = api.env('test')
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-flow-strip-types',
+    'inline-svg',
+    'emotion',
+  ]
+
+  if (isTest) {
+    plugins.push('@babel/plugin-transform-runtime')
+  }
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            ie: '11',
+            edge: '16',
+            firefox: '60',
+            chrome: '65',
+            safari: '10',
+            node: '8',
+          },
+          loose: true,
+        },
+      ],
+      '@babel/preset-flow',
+      '@babel/react',
+    ],
+    plugins: plugins,
+  }
+}
