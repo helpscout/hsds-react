aa5428258f7140ba0d4bedd222af28714d49e0b1
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2019-04-29 18:05:05 +0200

    Process ProgressBar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,20 +1,19 @@
-// @flow
-import type { UISize } from '../../constants/types'
-import React, { PureComponent as Component } from 'react'
+import * as React from 'react'
+import { UISize } from '../../constants/types'
 import { classNames } from '../../utilities/classNames'
 import { noop } from '../../utilities/other'
 
 type Value = number | string | null
 
 type Props = {
-  className?: string,
-  description?: string,
-  onChange: (value: Value) => void,
-  size?: UISize,
-  value: Value,
+  className?: string
+  description?: string
+  onChange: (value: Value) => void
+  size?: UISize
+  value: Value
 }
 
-class ProgressBar extends Component<Props> {
+class ProgressBar extends React.PureComponent<Props> {
   static defaultProps = {
     onChange: noop,
     value: 0,
@@ -28,6 +27,8 @@ class ProgressBar extends Component<Props> {
 
   getValue(val: Value): number {
     const value = val !== null ? val : this.props.value
+    // TODO: fix typescript complains
+    // @ts-ignore
     const barValue = parseFloat(value)
     const normalizedBarValue =
       barValue > 100 ? 100 : barValue < 0 ? 0 : barValue
@@ -55,8 +56,14 @@ class ProgressBar extends Component<Props> {
       <div
         className={componentClassName}
         role="progressbar"
+        // TODO: fix typescript complains
+        // @ts-ignore
         aria-valuenow={value}
+        // TODO: fix typescript complains
+        // @ts-ignore
         aria-valuemin="0"
+        // TODO: fix typescript complains
+        // @ts-ignore
         aria-valuemax="100"
         aria-valuetext={description}
         {...rest}
