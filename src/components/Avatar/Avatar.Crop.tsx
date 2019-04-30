832a459ac56d08ba240f66163b1358b490c74d00
Jon Q <hello@jonquach.com>
2019-04-30 16:14:23 -0400

    Avatar: Remove background when image is loaded
    This update adjusts the `Avatar` component to remove the background color
    once the Image is loaded in. This prevents unexpected cropping issues
    around the edge of rounded/circular avatars.

--- src/components/Avatar/Avatar.Crop.tsx
+++ src/components/Avatar/Avatar.Crop.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import { getEasingTiming } from '../../utilities/easing'
 import { classNames } from '../../utilities/classNames'
 import { CropUI } from './Avatar.css'
 
 export const AvatarCrop = props => {
-  const { className, children, hasImage, isImageLoaded, withShadow } = props
+  const {
+    animationDuration,
+    animationEasing,
+    className,
+    children,
+    hasImage,
+    isImageLoaded,
+    withShadow,
+  } = props
 
   const componentClassName = classNames(
     'c-Avatar__crop',
@@ -13,7 +22,10 @@ export const AvatarCrop = props => {
   )
 
   const styles = {
-    backgroundColor: hasImage ? 'currentColor' : null,
+    backgroundColor: isImageLoaded ? 'transparent' : 'currentColor',
+    transition: `background-color ${animationDuration}ms ${getEasingTiming(
+      animationEasing
+    )}`,
   }
 
   return (
@@ -24,6 +36,8 @@ export const AvatarCrop = props => {
 }
 
 AvatarCrop.defaultProps = {
+  animationDuration: 160,
+  animationEasing: 'ease',
   hasImage: false,
   isImageLoaded: false,
   withShadow: false,
