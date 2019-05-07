68953f680d1fb9cdbbe6c5fc31cc3269ba783fe1
Jon Q <hello@jonquach.com>
2019-05-07 16:10:39 -0400

    Fix popper import/export

--- src/utilities/popper.lib.js
+++ src/utilities/popper.lib.js
@@ -1,6 +1,8 @@
 /**
+ * Do not convert this file to TypeScript!
+ *
  * This exists to ensure that the imports work correctly, due to the naming
- * of the popper.js node_module.
+ * of the popper.js node_module, and how it is parsed by TypeScript.
  */
 import PopperJS from 'popper.js'
 
