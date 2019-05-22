23c167fb7f0e38b27532efb1ca732046728f4f27
Jon Q <hello@jonquach.com>
2019-05-22 16:20:33 -0400

    Refactor Centralize with innerRef

--- src/components/Centralize/Centralize.tsx
+++ src/components/Centralize/Centralize.tsx
@@ -2,15 +2,21 @@ import * as React from 'react'
 import getValidProps from '@helpscout/react-utils/dist/getValidProps'
 import propConnect from '../PropProvider/propConnect'
 import { classNames } from '../../utilities/classNames'
+import { noop } from '../../utilities/other'
 import { CentralizeUI } from './styles/Centralize.css'
 import { COMPONENT_KEY } from './Centralize.utils'
 
 export interface Props {
   className?: string
   children?: any
+  innerRef: (node: HTMLDivElement) => void
 }
 
 export class Centralize extends React.PureComponent<Props> {
+  static defaultProps = {
+    innerRef: noop,
+  }
+
   static className = 'c-Centralize'
 
   getClassName() {
@@ -20,10 +26,14 @@ export class Centralize extends React.PureComponent<Props> {
   }
 
   render() {
-    const { children, ...rest } = this.props
+    const { children, innerRef, ...rest } = this.props
 
     return (
-      <CentralizeUI {...getValidProps(rest)} className={this.getClassName()}>
+      <CentralizeUI
+        {...getValidProps(rest)}
+        className={this.getClassName()}
+        innerRef={innerRef}
+      >
         {children}
       </CentralizeUI>
     )
