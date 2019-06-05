515bfa719095fdfcbf8046253fd0dd1e96ca6301
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2019-06-05 16:34:37 +0200

    moved from extend to plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,8 @@ module.exports = {
     jest: true,
     node: true,
   },
-  plugins: ['jsx-a11y', 'react'],
+  plugins: ['jsx-a11y', 'react', 'import', '@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
