030e5aa13973aec53f6c93c01c16eacf93680bea
Jon Quach <hello@jonquach.com>
2019-06-17 18:32:43 -0400

    Styled: Rollback extra styles (#645)
    * Removing extra from styled. Rolling back Fancy to v2.5.0
    
    * 2.47.1-0
    
    * Revert "Removing extra from styled. Rolling back Fancy to v2.5.0"
    
    This reverts commit 1174832a9e2113947cd1ea2082939a816bb78ace.
    
    * Revert "Styled: Adding "Extras" (#629)"
    
    This reverts commit 03cc36fb01bc12c967fe79088a8ab3116f5074be.

--- src/@types/helpscout__fancy/index.d.ts
+++ src/@types/helpscout__fancy/index.d.ts
@@ -1,6 +1,5 @@
 declare module '@helpscout/fancy' {
   function styled(component: any, options?: any): any
   export function ThemeProvider(component: any): any
-  export function createStyled(...args: any): any
   export default styled
 }
