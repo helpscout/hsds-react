539b331bd5f46830efafe03a721795d56d890be3
Terrence L. Jones <terrenceljones805@gmail.com>
2019-06-19 19:43:24 -0700

    Some Emoji Picker component refinements

--- stories/EmojiPicker.stories.js
+++ stories/EmojiPicker.stories.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import Dropdown from '../src/components/Dropdown/V2'
-import { emojiSet } from '../src/components/'
 import EmojiPicker from '../src/components/EmojiPicker'
-import EmojiItem from '../src/components/EmojiPicker/EmojiPicker.Item'
 import EmojiPickerReadme from '../src/components/EmojiPicker/README.md'
 import { jsxDecorator } from 'storybook-addon-jsx'
 
 import { select } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 import { withArtboard } from '@helpscout/artboard'
 
 const stories = storiesOf('EmojiPicker', module)
@@ -25,15 +23,15 @@ stories.addParameters({
   readme: { sidebar: EmojiPickerReadme },
 })
 
-const onSelect = (item, props) => {
-  console.log('Selected Item', props.item)
-}
-
 stories.add('Default', () => {
   const props = {
     isOpen: true,
-    onSelect,
-    size: select('size', { default: 'default', sm: 'sm', lg: 'lg' }, 'default'),
+    onSelect: action('Emoji Selected'),
+    size: select(
+      'Emoji Size',
+      { default: 'default', sm: 'sm', lg: 'lg' },
+      'default'
+    ),
   }
 
   return <EmojiPicker {...props} />
