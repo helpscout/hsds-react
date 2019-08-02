fc222083ac3b3242782e07eab7c9564a558bd28f
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2019-08-02 12:44:32 +0200

    Fixes the IntersectionObserver guard for Safari 10, others

--- src/components/Page/Page.utils.ts
+++ src/components/Page/Page.utils.ts
@@ -7,3 +7,12 @@ export const COMPONENT_KEY = {
   Heading: 'PageHeading',
   Section: 'PageSection',
 }
+
+export function isIntersectionObserverSupported() {
+  /* istanbul ignore next */
+  if (typeof window === 'object' && 'IntersectionObserver' in window) {
+    return true
+  }
+  /* istanbul ignore next */
+  return false
+}
