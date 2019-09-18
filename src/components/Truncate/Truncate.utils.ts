96bf941b44d50ef26a303f388dac911d50c3acff
Corey Nilan <corey@helpscout.com>
2019-09-18 13:14:45 -0400

    Update styles to use BEM naming conventions.

--- src/components/Truncate/Truncate.utils.ts
+++ src/components/Truncate/Truncate.utils.ts
@@ -1,9 +1,9 @@
-export const TRUNCATED_COMPONENT_KEY = 'Truncated'
+export const TRUNCATED_COMPONENT_KEY = 'c-Truncate__content__chunks'
 
 export const TRUNCATED_CLASSNAMES = {
   component: TRUNCATED_COMPONENT_KEY,
-  withSplitter: 'withSplitter',
-  firstChunk: `${TRUNCATED_COMPONENT_KEY}__firstChunk`,
-  splitterChunk: `${TRUNCATED_COMPONENT_KEY}__splitterChunk`,
-  secondChunk: `${TRUNCATED_COMPONENT_KEY}__secondChunk`,
+  withSplitter: 'with-splitter',
+  firstChunk: `${TRUNCATED_COMPONENT_KEY}__first`,
+  splitterChunk: `${TRUNCATED_COMPONENT_KEY}__splitter`,
+  secondChunk: `${TRUNCATED_COMPONENT_KEY}__second`,
 }
