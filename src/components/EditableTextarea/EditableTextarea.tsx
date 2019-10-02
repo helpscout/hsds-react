04aa4fadd55ba086e33433946326adbb16c2a7c5
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2019-10-02 21:34:27 +0900

    trigger ci

--- src/components/EditableTextarea/EditableTextarea.tsx
+++ src/components/EditableTextarea/EditableTextarea.tsx
@@ -80,6 +80,7 @@ export class EditableTextarea extends React.PureComponent<
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.value === this.props.value) return
+
     // Tested
     /* istanbul ignore next */ if (nextProps.value === this.state.value) return
 
