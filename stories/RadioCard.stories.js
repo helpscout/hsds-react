87464ddea87ec23b44a4796e75990f5c26f1c553
Pierre-Luc Babin <plbabin@gmail.com>
2019-10-07 15:53:42 -0400

    Add content, heading and maxWidth to the RadioCard component (#721)
    * Add content, heading and maxWidth to the RadioCard component
    
    * Fix issue with rendering custom icon. Improve RadioCard types
    
    * Fix tests
    
    * Fix coverage

--- stories/RadioCard.stories.js
+++ stories/RadioCard.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { ChoiceGroup, RadioCard } from '../src/index'
+import { ChoiceGroup, RadioCard, Icon } from '../src/index'
 
 const stories = storiesOf('RadioCard', module)
 
@@ -60,5 +60,47 @@ stories.add('focused', () => (
     <RadioCard icon="fab-buoy" value="buoy" />
     <RadioCard icon="fab-search" value="search" />
     <RadioCard icon="fab-question" value="question" />
+  </ChoiceGroup>
+))
+
+stories.add('custom icons', () => (
+  <ChoiceGroup align="horizontal" onChange={onChange} value="chat">
+    <RadioCard icon={<Icon size="24" name="search" />} value="chat" />
+    <RadioCard icon={<Icon size="24" name="search" />} value="antenna" />
+    <RadioCard icon={<Icon size="24" name="fab-antenna" />} value="buoy" />
+    <RadioCard icon={<Icon size="24" name="search" />} value="search" />
+    <RadioCard icon={<Icon size="24" name="search" />} value="question" />
+  </ChoiceGroup>
+))
+
+stories.add('size, heading & content', () => (
+  <ChoiceGroup
+    align="horizontal"
+    onChange={onChange}
+    value="chat"
+    choiceMaxWidth="214px"
+    isResponsive
+  >
+    <RadioCard
+      icon="fab-chat"
+      content="some content"
+      heading="Chat"
+      value="chat"
+      maxWidth="214px"
+    />
+    <RadioCard
+      icon="fab-antenna"
+      content="some content"
+      heading="Antenna"
+      value="antenna"
+      maxWidth="214px"
+    />
+    <RadioCard
+      icon="fab-buoy"
+      content="some content"
+      heading="Buoy"
+      value="buoy"
+      maxWidth="214px"
+    />
   </ChoiceGroup>
 ))
