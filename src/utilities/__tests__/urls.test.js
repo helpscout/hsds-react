d807c230a3895150b7c1e76b42189bcd0ddd219e
Corey Nilan <corey@helpscout.com>
2019-10-15 15:05:04 -0400

    Add tests

--- src/utilities/__tests__/urls.test.js
+++ src/utilities/__tests__/urls.test.js
@@ -1,6 +1,9 @@
 import { normalizeUrl } from '../urls'
 
 describe('normalizeUrl', () => {
+  test('returns an empty string if nothing is provided', () => {
+    expect(normalizeUrl()).toEqual('')
+  })
   test('Adds the "http://" scheme if necessary', () => {
     const urls = [
       ['www.example.com', 'http://www.example.com'],
