710b6593e0f46f666f9c533420b40e1653370ee7
Corey Nilan <corey@helpscout.com>
2019-11-15 18:09:42 -0500

    Build out Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import * as React from 'react'
+import Actions from './Form.Actions'
+import { classNames } from '../../utilities/classNames'
 
-const Form = ({ children }) => {
-  return <form>{children}</form>
+export class Form extends React.PureComponent {
+  static Actions = Actions
+
+  render() {
+    const { children, className } = this.props
+
+    const componentClassName = classNames('c-Form', className)
+
+    return <form className={componentClassName}>{children}</form>
+  }
 }
 
 export default Form
