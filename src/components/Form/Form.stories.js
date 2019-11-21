6bf4910c99cdf28128b68dd8921f5e7d703685f2
Corey Nilan <corey@helpscout.com>
2019-11-21 13:41:02 -0500

    HSAPP-734 Create a Form component (#753)
    * Skeleton of component, docs, tests - can be run in Storybook
    
    * Build out Form component
    
    * Build out Form.Actions
    
    * Add tests, documentation, story
    
    * Add Form.Actions to Form component
    
    * documentation
    
    * add knobs for story
    
    * add readme to storybook

---
+++ src/components/Form/Form.stories.js
@@ -0,0 +1,208 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action as addonAction } from '@storybook/addon-actions'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import styled from '../styled'
+import { Form, FormGroup, FormLabel, Input } from '../index'
+import Readme from './README.md'
+
+const action = name => (...args) => {
+  addonAction(name)(...args)
+  console.log(name, { args })
+}
+
+export const ContainerUI = styled('div')`
+  form {
+    max-width: 60%;
+  }
+`
+
+const stories = storiesOf('Form', module)
+
+stories.addDecorator(withKnobs).addParameters({ readme: { sidebar: Readme } })
+
+stories.add('with save, cancel and delete buttons', () => {
+  class SampleForm extends React.Component {
+    state = {
+      text: 'Sample Text',
+    }
+
+    handleChange = value => {
+      this.setState({
+        text: value,
+      })
+    }
+
+    handleFormSubmit = evt => {
+      evt.preventDefault()
+      console.log(`Submitting text: "${this.state.text}"`)
+      this.setState({ text: '' })
+    }
+
+    handleCancel = () => {
+      console.log('cancelling!')
+      this.setState({ text: '' })
+    }
+
+    handleDestroy = () => {
+      console.log('deleting!')
+      this.setState({ text: '' })
+    }
+    render() {
+      return (
+        <ContainerUI>
+          <Form
+            actionDirection={text('actionDirection', 'left')}
+            cancelText="Cancel Changes"
+            destroyText="Delete Item"
+            onCancel={this.handleCancel}
+            onDestroy={this.handleDestroy}
+            onSave={this.handleFormSubmit}
+            saveText="Save Entry"
+          >
+            <FormGroup>
+              <FormLabel label="Site Name">
+                <Input onChange={this.handleChange} value={this.state.text} />
+              </FormLabel>
+            </FormGroup>
+          </Form>
+        </ContainerUI>
+      )
+    }
+  }
+
+  return <SampleForm />
+})
+
+stories.add('with save and cancel buttons', () => {
+  class SampleForm extends React.Component {
+    state = {
+      text: 'Sample Text',
+    }
+
+    handleChange = value => {
+      this.setState({
+        text: value,
+      })
+    }
+
+    handleFormSubmit = evt => {
+      evt.preventDefault()
+      console.log(`Submitting text: "${this.state.text}"`)
+      this.setState({ text: '' })
+    }
+
+    handleCancel = () => {
+      console.log('cancelling!')
+      this.setState({ text: '' })
+    }
+
+    render() {
+      return (
+        <ContainerUI>
+          <Form
+            actionDirection={text('actionDirection', 'left')}
+            cancelText="Cancel Changes"
+            onCancel={this.handleCancel}
+            onSave={this.handleFormSubmit}
+            saveText="Save Entry"
+          >
+            <FormGroup>
+              <FormLabel label="Site Name">
+                <Input onChange={this.handleChange} value={this.state.text} />
+              </FormLabel>
+            </FormGroup>
+          </Form>
+        </ContainerUI>
+      )
+    }
+  }
+
+  return <SampleForm />
+})
+
+stories.add('with save and delete buttons', () => {
+  class SampleForm extends React.Component {
+    state = {
+      text: 'Sample Text',
+    }
+
+    handleChange = value => {
+      this.setState({
+        text: value,
+      })
+    }
+
+    handleFormSubmit = evt => {
+      evt.preventDefault()
+      console.log(`Submitting text: "${this.state.text}"`)
+      this.setState({ text: '' })
+    }
+
+    handleDestroy = () => {
+      console.log('deleting!')
+      this.setState({ text: '' })
+    }
+    render() {
+      return (
+        <ContainerUI>
+          <Form
+            actionDirection={text('actionDirection', 'left')}
+            destroyText="Delete Item"
+            onDestroy={this.handleDestroy}
+            onSave={this.handleFormSubmit}
+            saveText="Save Entry"
+          >
+            <FormGroup>
+              <FormLabel label="Site Name">
+                <Input onChange={this.handleChange} value={this.state.text} />
+              </FormLabel>
+            </FormGroup>
+          </Form>
+        </ContainerUI>
+      )
+    }
+  }
+
+  return <SampleForm />
+})
+
+stories.add('with only save button', () => {
+  class SampleForm extends React.Component {
+    state = {
+      text: 'Sample Text',
+    }
+
+    handleChange = value => {
+      this.setState({
+        text: value,
+      })
+    }
+
+    handleFormSubmit = evt => {
+      evt.preventDefault()
+      console.log(`Submitting text: "${this.state.text}"`)
+      this.setState({ text: '' })
+    }
+
+    render() {
+      return (
+        <ContainerUI>
+          <Form
+            actionDirection={text('actionDirection', 'left')}
+            onSave={this.handleFormSubmit}
+            saveText="Save Entry"
+          >
+            <FormGroup>
+              <FormLabel label="Site Name">
+                <Input onChange={this.handleChange} value={this.state.text} />
+              </FormLabel>
+            </FormGroup>
+          </Form>
+        </ContainerUI>
+      )
+    }
+  }
+
+  return <SampleForm />
+})
---
+++ src/components/Form/Form.test.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Form from './Form'
+
+describe('ClassName', () => {
+  test('Has default className', () => {
+    const wrapper = mount(<Form />)
+
+    expect(wrapper.getDOMNode().classList.contains('c-Form')).toBeTruthy()
+  })
+
+  test('Applies custom className if specified', () => {
+    const customClass = 'schrute'
+    const wrapper = mount(<Form className={customClass} />)
+
+    expect(wrapper.getDOMNode().classList.contains(customClass)).toBeTruthy()
+  })
+})
+
+describe('Children', () => {
+  test('Renders child content', () => {
+    const text = 'Hello'
+    const wrapper = mount(
+      <Form>
+        <div className="child">{text}</div>
+      </Form>
+    )
+    const el = wrapper.find('div.child')
+
+    expect(el.text()).toContain(text)
+  })
+})
+
+describe.only('Actions', () => {
+  test('Renders only a save button by default with default text', () => {
+    const wrapper = mount(<Form />)
+    const el = wrapper.find('.save-button').hostNodes()
+
+    expect(el.text()).toContain('Save')
+    expect(wrapper.find('button').length).toEqual(1)
+  })
+
+  test('Renders a save button with custom text if specified', () => {
+    const text = 'Save Entry'
+    const wrapper = mount(<Form saveText={text} />)
+    const el = wrapper.find('.save-button').hostNodes()
+
+    expect(el.text()).toEqual(text)
+  })
+
+  test('Renders a cancel button if specified', () => {
+    const wrapper = mount(<Form onCancel={() => {}} />)
+
+    const el = wrapper.find('.cancel-button').hostNodes()
+
+    expect(el.text()).toEqual('Cancel')
+    expect(wrapper.find('button').length).toEqual(2)
+  })
+
+  test('Renders a delete button if specified', () => {
+    const wrapper = mount(<Form onDestroy={() => {}} />)
+    const el = wrapper.find('.delete-button').hostNodes()
+
+    expect(el.text()).toEqual('Delete')
+    expect(wrapper.find('button').length).toEqual(2)
+  })
+
+  test('Renders buttons from left to right by default', () => {
+    const wrapper = mount(<Form />)
+
+    expect(wrapper.find('.is-right').hostNodes()).toHaveLength(1)
+    expect(wrapper.find('.is-left').hostNodes()).toHaveLength(0)
+  })
+
+  test('Renders buttons right to left when specified', () => {
+    const wrapper = mount(<Form actionDirection="left" />)
+
+    expect(wrapper.find('.is-right').hostNodes()).toHaveLength(0)
+    expect(wrapper.find('.is-left').hostNodes()).toHaveLength(1)
+  })
+})
