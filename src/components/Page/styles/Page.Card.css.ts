1e385ac05ad3904f3e6a16986dd49321e2127eee
Tom O'Connor <tjbo@users.noreply.github.com>
2020-01-09 11:01:27 -0500

    Tweak page card bottom margin (#767)
    Change bottom margin on the PageCard component to 20px.

--- src/components/Page/styles/Page.Card.css.ts
+++ src/components/Page/styles/Page.Card.css.ts
@@ -7,20 +7,20 @@ import PageConfig from './Page.config.css'
 export const config = {
   borderRadius: '4px',
   boxShadow: `
-    0 0 0 1px rgba(0, 0, 0, 0.04), 
-    0 2px 8px 0 rgba(0,0,0,0.04), 
+    0 0 0 1px rgba(0, 0, 0, 0.04),
+    0 2px 8px 0 rgba(0,0,0,0.04),
     0 5px 10px 0 rgba(99, 116, 134, 0.03)
   `,
   boxShadowHover: `
-    rgb(214, 221, 227) 0px 0px 0px 1px, 
-    0 2px 8px 0 rgba(0,0,0,0.04), 
+    rgb(214, 221, 227) 0px 0px 0px 1px,
+    0 2px 8px 0 rgba(0,0,0,0.04),
     0 5px 10px 0 rgba(99, 116, 134, 0.03)
   `,
   flexDirection: {
     default: 'column',
     superWidescreen: 'row',
   },
-  marginBottom: '40px',
+  marginBottom: '20px',
   padding: {
     default: '50px 50px 65px',
     widescreen: '50px 100px 65px',
