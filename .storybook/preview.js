4b796a1971998c0eb5a27fcc0da336a01f072863
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2020-01-14 16:22:30 +0100

    Updates storybook to latest (5.3.3)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import {
-  addDecorator,
-  configure,
-  addParameters,
-  setAddon,
-} from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { create } from '@storybook/theming'
 import { withKnobs } from '@storybook/addon-knobs'
-import { StatsGraph } from '@helpscout/stats'
-import { addReadme } from 'storybook-readme'
 import { withA11y } from '@storybook/addon-a11y'
 
 import '../src/adapters/app'
@@ -17,18 +10,9 @@ import './storybook.css'
 
 import HSDS from '../src/components/HSDS'
 
-const withStats = storyFn => (
-  <div>
-    <StatsGraph right={5} opacity={0.2} />
-    {storyFn()}
-  </div>
-)
-
 const withHSDSScope = storyFn => <HSDS.Provider>{storyFn()}</HSDS.Provider>
 
-addDecorator(withStats)
 addDecorator(withKnobs)
-addDecorator(addReadme)
 addDecorator(withA11y)
 addDecorator(withHSDSScope)
 
@@ -44,24 +28,9 @@ addParameters({
     panelPosition: 'bottom',
     isToolshown: true,
   },
-  readme: {
-    codeTheme: 'a11y-dark',
-  },
   viewport: {
     viewports: {
       ...INITIAL_VIEWPORTS,
     },
   },
 })
-
-// automatically import all files ending in *.stories.js
-function loadStories() {
-  const reqs = [
-    require.context('../stories', true, /.stories.(js|ts|tsx)$/),
-    require.context('../src', true, /.stories.(js|ts|tsx)$/),
-  ]
-
-  reqs.forEach(req => req.keys().forEach(filename => req(filename)))
-}
-
-configure(loadStories, module)
