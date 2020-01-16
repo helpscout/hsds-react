59998149249223a74718f2cea335a30ab762c7ea
tjbo <tomoconnor@helpscout.com>
2020-01-16 17:47:41 -0500

    Fix formatting

--- src/components/CopyInput/styles/CopyInput.css.js
+++ src/components/CopyInput/styles/CopyInput.css.js
@@ -4,10 +4,10 @@ import Input from '../../Input'
 
 export const CopyInputUI = styled(Input)`
   input::-moz-selection {
-    background: ${getColor('blue.200')};	    background: ${getColor('blue.200')};
-  }	  }
+    background: ${getColor('blue.200')};
+  }
 
   input::selection {
-    background: ${getColor('blue.200')};	    background: ${getColor('blue.200')};
-  }	  }
+    background: ${getColor('blue.200')};
+  }
 `
