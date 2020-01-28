721aa19f3cd6bd395f86e4432ef72f5e4db292e0
Brit Young <brit@helpscout.com>
2020-01-28 12:49:48 -0800

    make tooltip default zIndex higher for use in modals

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -43,7 +43,7 @@ export class Tooltip extends React.PureComponent<Props> {
     modifiers: {},
     placement: 'top',
     triggerOn: 'hover',
-    zIndex: 999,
+    zIndex: 9999,
   }
 
   static className = 'c-Tooltip'
