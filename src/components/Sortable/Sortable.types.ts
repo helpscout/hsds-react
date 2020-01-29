f866ed326f3bc06e6bdd4983137d6ae52d327964
Nickolas Kenyeres <nickolas@helpscout.com>
2020-01-29 15:49:18 -0500

    Make props optional
    These props are not required

--- src/components/Sortable/Sortable.types.ts
+++ src/components/Sortable/Sortable.types.ts
@@ -1,22 +1,22 @@
 export interface SortableProps {
-  axis: 'x' | 'y' | 'xy'
-  className: string
-  distance: number
-  lockAxis: string
-  helperClass: string
-  hideDragHandles: boolean
-  transitionDuration: number
-  contentWindow: any
-  onSortStart: () => void
-  onSortMove: () => void
-  onSortEnd: (...args: any[]) => void
-  shouldCancelStart: () => void
-  pressDelay: number
-  useDragHandle: boolean
-  useWindowAsScrollContainer: boolean
-  hideSortableGhost: boolean
-  lockToContainerEdges: boolean
-  lockOffset: number | string | Array<string | number>
-  getContainer: () => void
-  getHelperDimensions: () => void
+  axis?: 'x' | 'y' | 'xy'
+  className?: string
+  distance?: number
+  lockAxis?: string
+  helperClass?: string
+  hideDragHandles?: boolean
+  transitionDuration?: number
+  contentWindow?: any
+  onSortStart?: () => void
+  onSortMove?: () => void
+  onSortEnd?: (...args: any[]) => void
+  shouldCancelStart?: () => void
+  pressDelay?: number
+  useDragHandle?: boolean
+  useWindowAsScrollContainer?: boolean
+  hideSortableGhost?: boolean
+  lockToContainerEdges?: boolean
+  lockOffset?: number | string | Array<string | number>
+  getContainer?: () => void
+  getHelperDimensions?: () => void
 }
