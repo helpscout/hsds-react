ab3633d99daa7ae983270da8dcc678b24cebbc7e
Nickolas Kenyeres <nickolas@knicklabs.com>
2020-02-07 17:04:29 -0500

    Prevent Data from Being Retained when Removing the Last Field Value in an EditableField (#787)
    * Prevent data from sticking around when resetting field.
    
    * Appease TypeScript
    
    * 2.86.4-0
    
    * Revert changes from release

--- src/components/EditableField/EditableField.tsx
+++ src/components/EditableField/EditableField.tsx
@@ -882,8 +882,10 @@ export class EditableField extends React.Component<
     // Clearing value
     // When there is only one item in the fieldValue array
     if (fieldValue.length === 1) {
-      const emptyValue = {
-        ...fieldValue[0],
+      const emptyValue: FieldValue = {
+        // The id is the input id and we will need to keep this to recycle
+        // the field, but we need to drop all other data associated with it.
+        id: fieldValue[0].id,
         value: EMPTY_VALUE,
         validated: false,
       }
