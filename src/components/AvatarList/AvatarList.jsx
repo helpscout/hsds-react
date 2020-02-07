aaf349887af6a89aa5f19cdcb4e6dfb9b0a313fb
Pierre-Luc Babin <plbabin@gmail.com>
2020-02-07 18:22:54 +0000

    Refactor AvatarList to support multiple layout

--- src/components/AvatarList/AvatarList.jsx
+++ src/components/AvatarList/AvatarList.jsx
@@ -3,16 +3,36 @@ import { PropTypes } from 'prop-types'
 import getValidProps from '@helpscout/react-utils/dist/getValidProps'
 import Avatar from '../Avatar'
 import Animate from '../Animate'
-import List from '../List'
+import { ItemUI, AvatarListUI, AvatarListWrapperUI } from './AvatarList.css'
 import { classNames } from '../../utilities/classNames'
 import { getComponentKey } from '../../utilities/component'
+import { isOdd, getMiddleIndex } from '../../utilities/number'
 
 export const AvatarListContext = React.createContext({})
 
 const wrapAvatar = (props, avatar, index) => {
-  const { animationDuration, animationEasing, animationSequence } = props
+  const {
+    animationDuration,
+    animationEasing,
+    animationSequence,
+    max,
+    stack,
+    count,
+  } = props
+
+  let zIndex = max - index
+
+  if (stack && count > 2 && isOdd(`${count}`)) {
+    if (isOdd(index)) {
+      zIndex = zIndex + 1
+    }
+    if (index === getMiddleIndex(`${count}`)) {
+      zIndex = zIndex + 2
+    }
+  }
+
   return (
-    <List.Item key={getComponentKey(avatar, index)}>
+    <ItemUI key={getComponentKey(avatar, index)} zIndex={zIndex}>
       <Animate
         className="c-AvatarList__item"
         duration={animationDuration}
@@ -21,55 +41,76 @@ const wrapAvatar = (props, avatar, index) => {
       >
         {avatar}
       </Animate>
-    </List.Item>
+    </ItemUI>
   )
 }
 
+const getCurrentCount = ({ count, max }) => (count < max ? count : max)
+
+const getAvatarSize = ({ size: sizeProp, stack, ...rest }) => {
+  const currentCount = getCurrentCount(rest)
+
+  if (!stack) return sizeProp
+
+  let size = 'md'
+
+  if (currentCount === 2) {
+    size = 'lg'
+  }
+  if (currentCount === 1) {
+    size = 'xl'
+  }
+
+  return size
+}
+
 export const AvatarList = props => {
-  const { children, max, size, className, center, ...rest } = props
+  const { children, max, className, center, stack, grid, ...rest } = props
   const avatars = React.Children.toArray(children)
   const avatarList =
-    max && avatars.length > max ? avatars.slice(0, max - 1) : avatars
+    max && avatars.length > max ? avatars.slice(0, max) : avatars
   const extraAvatarCount = avatars.length - avatarList.length
   const shouldShowExtra = extraAvatarCount > 0
+
+  const propsWithCount = { ...props, count: avatars.length }
+
+  const size = getAvatarSize(propsWithCount)
 
   const contextValue = { size }
 
   const avatarComponents = avatarList.map((avatar, index) => {
     //TODO: stacking
-    return wrapAvatar(props, avatar, index)
+    return wrapAvatar(propsWithCount, avatar, index)
   })
 
   if (shouldShowExtra) {
     const extraLabel = `+${extraAvatarCount}`
     avatarComponents.push(
       wrapAvatar(
-        props,
+        propsWithCount,
         <Avatar count={extraLabel} light name={extraLabel} />,
         avatarList.length
       )
     )
   }
-  const componentClassName = classNames('c-AvatarList', className)
+  const componentClassName = classNames(
+    'c-AvatarList',
+    stack && 'is-withLayerStack',
+    grid && 'is-grid',
+    className
+  )
   const componentWrapperClassName = classNames(
     'c-AvatarListWrapper',
     center && 'is-center'
   )
   return (
-    <div className={componentWrapperClassName}>
+    <AvatarListWrapperUI className={componentWrapperClassName}>
       <AvatarListContext.Provider value={contextValue}>
-        <List
-          {...getValidProps(rest)}
-          className={componentClassName}
-          display="flex"
-          inlineSize="sm"
-          type="inline"
-          size="xs"
-        >
+        <AvatarListUI {...getValidProps(rest)} className={componentClassName}>
           {avatarComponents}
-        </List>
+        </AvatarListUI>
       </AvatarListContext.Provider>
-    </div>
+    </AvatarListWrapperUI>
   )
 }
 
@@ -81,7 +122,7 @@ AvatarList.propTypes = {
   borderColor: PropTypes.string,
   className: PropTypes.string,
   max: PropTypes.number,
-  multiline: PropTypes.bool,
+  grid: PropTypes.bool,
   size: PropTypes.string,
   stack: PropTypes.bool,
   center: PropTypes.bool,
@@ -91,7 +132,7 @@ AvatarList.defaultProps = {
   animationEasing: 'ease',
   animationSequence: 'fade',
   max: 4,
-  multiline: false,
+  grid: false,
   center: false,
   showStatusBorderColor: false,
   size: 'sm',
