d79e187a6e354c795871ad975f55ab7414d2d0bc
Terrence L. Jones <terrenceljones805@gmail.com>
2020-02-19 16:44:18 -0600

    Add textIncludesOnlyEmoji string util

--- src/utilities/strings.ts
+++ src/utilities/strings.ts
@@ -35,6 +35,22 @@ export const wordHasSpaces = (word: string | any): boolean => {
   return !!(isWordString(word) && word.trim().indexOf(' ') > 0)
 }
 
+export const textIncludesOnlyEmoji = text => {
+  // Note: `\u2028`& `\uFE0F` are not emoji characters, but instead unicode line
+  // separators that seem to appear when the `replace` methods are called below.
+  const emojiPattern = /(\u2028|\uFE0F|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g
+
+  if (!isWordString(text)) return false
+
+  // Remove all emoji and see if any characters remain
+  const withEmojisRemoved = text.replace(emojiPattern, '')
+
+  // Remove spaces
+  const withSpacesRemoved = withEmojisRemoved.replace(/[\s\n]/gm, '')
+
+  return !withSpacesRemoved
+}
+
 // Source
 // https://github.com/kahwee/truncate-middle
 export const truncateMiddle = (
