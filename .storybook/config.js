45178eb27ef55d23fb580e0513a2b3f8c429318d
Pierre-Luc Babin <plbabin@gmail.com>
2020-06-03 19:07:05 +0000

    Upgrade storybook to 5.3.19

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,27 +5,15 @@ import {
   addParameters,
   setAddon,
 } from '@storybook/react'
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { create } from '@storybook/theming'
 import { withKnobs } from '@storybook/addon-knobs'
-import { StatsGraph } from '@helpscout/stats'
-import { addReadme } from 'storybook-readme'
 import { withA11y } from '@storybook/addon-a11y'
 
 import '../src/adapters/app'
 import '../src/styles/blue.scss'
 import './storybook.css'
 
-const withStats = storyFn => (
-  <div>
-    <StatsGraph right={5} opacity={0.2} />
-    {storyFn()}
-  </div>
-)
-
-addDecorator(withStats)
 addDecorator(withKnobs)
-addDecorator(addReadme)
 addDecorator(withA11y)
 
 addParameters({
@@ -42,11 +30,6 @@ addParameters({
   },
   readme: {
     codeTheme: 'a11y-dark',
-  },
-  viewport: {
-    viewports: {
-      ...INITIAL_VIEWPORTS,
-    },
   },
 })
 
