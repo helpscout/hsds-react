69e5c98847e388831e73d88be13b909ea71eed12
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2020-06-24 12:46:03 +0200

    removes polyfill.js

--- config/polyfills.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-// Object.assign() is commonly used with React.
-// It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign')
-Object.values = require('object-values')
