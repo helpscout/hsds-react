77540731c9811a565f9eb35335ac7be9cc31dd11
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2020-09-16 14:03:38 +0200

    setup react testing library

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,5 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import '@testing-library/jest-dom'
 import { setupTests } from '@helpscout/cyan'
 
 Enzyme.configure({ adapter: new Adapter() })
