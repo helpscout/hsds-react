1c4543a8efd23961bdc623aa8ebe994dcba272ce
Pierre-Luc Babin <plbabin@gmail.com>
2020-09-28 17:50:41 +0000

    Remove emoji in story title. Improve stories sorting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,9 +16,12 @@ addDecorator(withHSDSScope)
 addParameters({
   options: {
     showRoots: true,
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind
-        ? 0
-        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    storySort: {
+      method: (a, b) =>
+        a[1].kind === b[1].kind
+          ? 0
+          : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      order: ['Welcome', 'Style', 'Components', 'Utilities'],
+    },
   },
 })
