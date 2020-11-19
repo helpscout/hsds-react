09d5fa46677b1b06829f8414b80df0b28cbf86aa
Juan Pablo Lomeli Diaz <tinkertrain@gmail.com>
2020-11-19 17:39:23 +0100

    Adds wrapper and innerRef

--- src/components/Datepicker/Datepicker.js
+++ src/components/Datepicker/Datepicker.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useDatepicker, START_DATE } from '@datepicker-react/hooks'
 import { noop } from '../../utilities/other'
@@ -16,8 +16,10 @@ function Datepicker({
   minBookingDays = 1,
   numberOfMonths = 1,
   onDateChange = noop,
+  innerRef = noop,
   startDate = null,
 }) {
+  const datePickerRef = useRef(null)
   const [dates, setDates] = useState({
     startDate: getJSDateFromString(startDate),
     endDate: getJSDateFromString(endDate) || getJSDateFromString(startDate),
@@ -25,6 +27,14 @@ function Datepicker({
   })
   const [prevDates, setPrevDates] = useState({})
   const [deepNavVisible, setDeepNavVisibility] = useState(false)
+
+  useEffect(() => {
+    if (datePickerRef && datePickerRef.current) {
+      innerRef(datePickerRef.current)
+    }
+    // No need to add innerRef prop to the deps list
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [datePickerRef])
 
   const {
     activeMonths,
@@ -117,39 +127,41 @@ function Datepicker({
   )
 
   return (
-    <DatepickerContext.Provider value={contextValue}>
-      <CalendarContainerUI
-        className="c-Calendar"
-        numberOfMonths={numberOfMonths}
-        role="dialog"
-      >
-        {!deepNavVisible ? (
-          <DailyCalendar
-            activeMonths={activeMonths}
-            allowFutureDatePick={allowFutureDatePick}
-            firstDayOfWeek={firstDayOfWeek}
-            goToPreviousMonth={goToPreviousMonthsByOneMonth}
-            goToNextMonth={goToNextMonthsByOneMonth}
-            numberOfMonths={numberOfMonths}
-            onDeepNavigationClick={() => {
-              setDeepNavVisibility(!deepNavVisible)
-            }}
-          />
-        ) : (
-          <PeriodCalendar
-            activeMonths={activeMonths}
-            allowFutureDatePick={allowFutureDatePick}
-            goToDate={(newDate, switchCalendar) => {
-              goToDate(newDate)
-              switchCalendar && setDeepNavVisibility(false)
-            }}
-            goToNextYear={goToNextYear}
-            goToPreviousYear={goToPreviousYear}
-            startDate={dates.startDate}
-          />
-        )}
-      </CalendarContainerUI>
-    </DatepickerContext.Provider>
+    <div className="DatepickerContainer" ref={datePickerRef}>
+      <DatepickerContext.Provider value={contextValue}>
+        <CalendarContainerUI
+          className="c-Calendar"
+          numberOfMonths={numberOfMonths}
+          role="dialog"
+        >
+          {!deepNavVisible ? (
+            <DailyCalendar
+              activeMonths={activeMonths}
+              allowFutureDatePick={allowFutureDatePick}
+              firstDayOfWeek={firstDayOfWeek}
+              goToPreviousMonth={goToPreviousMonthsByOneMonth}
+              goToNextMonth={goToNextMonthsByOneMonth}
+              numberOfMonths={numberOfMonths}
+              onDeepNavigationClick={() => {
+                setDeepNavVisibility(!deepNavVisible)
+              }}
+            />
+          ) : (
+            <PeriodCalendar
+              activeMonths={activeMonths}
+              allowFutureDatePick={allowFutureDatePick}
+              goToDate={(newDate, switchCalendar) => {
+                goToDate(newDate)
+                switchCalendar && setDeepNavVisibility(false)
+              }}
+              goToNextYear={goToNextYear}
+              goToPreviousYear={goToPreviousYear}
+              startDate={dates.startDate}
+            />
+          )}
+        </CalendarContainerUI>
+      </DatepickerContext.Provider>
+    </div>
   )
 }
 
@@ -162,6 +174,8 @@ Datepicker.propTypes = {
   endDate: PropTypes.instanceOf(Date),
   /** Which day of the week is first in the calendar (0 -> sunday, 1 -> monday, etc) */
   firstDayOfWeek: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
+  /** Retrieves the Datepicker most outer DOM node */
+  innerRef: PropTypes.func,
   /** How many days can be selected in a range (minimum). */
   minBookingDays: PropTypes.number,
   /** How many months to display at the same time. Note: Currently supported 1 */
