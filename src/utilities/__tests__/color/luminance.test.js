50a2313afeec069ff4a282d352dd461ddfa9c7b5
Maria Petrunina <maria.plyasunova@gmail.com>
2021-05-28 14:41:59 -0400

    Revert BEMBED-275: Helpscout chat modal color contrast (#934)

--- src/utilities/__tests__/color/luminance.test.js
+++
@@ -1,8 +0,0 @@
-import { luminance } from '../../color'
-
-test('Calculate contrast between two RGB colors', () => {
-  expect(luminance({ r: 0, g: 0, b: 0 })).toEqual(0)
-  expect(luminance({ r: 255, g: 255, b: 255 })).toEqual(1)
-  expect(+luminance({ r: 255, g: 10, b: 133 }).toFixed(2)).toEqual(0.23)
-  expect(+luminance({ r: 102, g: 102, b: 102 }).toFixed(2)).toEqual(0.13)
-})
