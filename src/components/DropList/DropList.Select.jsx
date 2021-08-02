04916c7dac9626406ae0a01158c98fb8d608af7e
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2021-08-02 16:23:39 +0200

    reduce timeout

--- src/components/DropList/DropList.Select.jsx
+++ src/components/DropList/DropList.Select.jsx
@@ -187,7 +187,7 @@ function Select({
                 ) {
                   toggleOpenedState(false)
                 }
-              }, 100)
+              }, 50)
             }
             onMenuBlur(e)
           },
