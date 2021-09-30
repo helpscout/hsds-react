9126a7695d06a306b050200bd2dbcbad65771af5
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2021-09-30 13:16:05 +0200

    extra

--- src/components/AvatarRow/AvatarRow.jsx
+++ src/components/AvatarRow/AvatarRow.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, useCallback } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import throttle from 'lodash.throttle'
 import classNames from 'classnames'
