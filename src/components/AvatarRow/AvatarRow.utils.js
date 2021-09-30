99b0fa60659b77d0e27de2402003775535cf2b65
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2021-09-30 12:27:12 +0200

    adds the resizable thing to test

--- src/components/AvatarRow/AvatarRow.utils.js
+++ src/components/AvatarRow/AvatarRow.utils.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AvatarRow from './AvatarRow'
 import { generateAvatarList } from '../../utilities/specs/avatar.specs'
-
+import { Resizable } from 're-resizable'
 export const MARGIN_LEFT = 2
 
 export function splitAvatarsArray(avatars, itemsDisplayed) {
@@ -35,16 +35,32 @@ export function setupObserver(callback) {
 }
 
 export const AvatarRowPlayground = () => {
-  const [avatars, setAvatars] = React.useState(generateAvatarList(10, true))
+  const [avatars, setAvatars] = React.useState(generateAvatarList(3, true))
 
   const handleClick = () => {
     setAvatars([...avatars, ...generateAvatarList(1, true)])
   }
 
   return (
-    <div>
-      <AvatarRow size="lg" gap={10} avatars={avatars} />
+    <>
+      <Resizable
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          outline: 'solid 1px #444cf770',
+          backgroundColor: '#e5e5f730',
+          backgroundImage: 'radial-gradient(#444cf770 0.5px, #e5e5f730 0.5px)',
+          backgroundSize: '10px 10px',
+        }}
+        defaultSize={{
+          width: '50%',
+          height: 200,
+        }}
+      >
+        <AvatarRow size="lg" gap={10} avatars={avatars} />
+      </Resizable>
       <button onClick={handleClick}>increase avatar count</button>
-    </div>
+    </>
   )
 }
