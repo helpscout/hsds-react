afc531d88907d0403b6ca3d0b7431136bb98fece
Pierre-Luc Babin <plbabin@gmail.com>
2021-11-15 19:39:47 +0000

    Rewrite copy to functional component

--- src/components/CopyButton/CopyButton.jsx
+++ src/components/CopyButton/CopyButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import getValidProps from '@helpscout/react-utils/dist/getValidProps'
 import Icon from '../Icon'
@@ -11,90 +11,65 @@ import {
   IconUI,
 } from './CopyButton.css'
 
-class CopyButton extends React.PureComponent {
-  state = {
-    shouldRenderConfirmation: false,
-  }
+const CopyButton = props => {
+  const { className, size, icon, label, onReset, resetTimeout, ...rest } = props
 
-  _isMounted = false
-  confirmationTimeout = null
+  const [shouldRenderConfirmation, setConfirmation] = useState()
+  const confirmationTimeout = useRef()
 
-  componentDidMount() {
-    this._isMounted = true
-  }
-
-  componentWillUnmount() {
-    this.clearConfirmationTimeout()
-    this._isMounted = false
-  }
-
-  safeSetState = state => {
-    if (this._isMounted) {
-      this.setState(state)
+  useEffect(() => {
+    if (confirmationTimeout.current) {
+      clearTimeout(confirmationTimeout)
     }
-  }
+    return () => {
+      if (confirmationTimeout.current) {
+        clearTimeout(confirmationTimeout)
+      }
+    }
+  })
 
-  startConfirmationTimeout = () => {
-    const { onReset, resetTimeout } = this.props
+  const handleClick = () => {
+    if (confirmationTimeout.current) {
+      clearTimeout(confirmationTimeout)
+    }
 
-    this.safeSetState({
-      shouldRenderConfirmation: true,
-    })
+    setConfirmation(true)
 
-    this.confirmationTimeout = setTimeout(() => {
-      this.safeSetState({
-        shouldRenderConfirmation: false,
-      })
+    confirmationTimeout.current = setTimeout(() => {
+      setConfirmation(false)
       onReset()
     }, resetTimeout)
   }
 
-  clearConfirmationTimeout = () => {
-    clearTimeout(this.confirmationTimeout)
-  }
+  const componentClassName = classNames(
+    'c-CopyButton',
+    shouldRenderConfirmation && 'is-copyConfirmed',
+    icon && 'is-with-icon',
+    className
+  )
+  const iconSize = size === 'sm' ? '20' : '24'
 
-  handleOnClick = event => {
-    this.clearConfirmationTimeout()
-    this.startConfirmationTimeout()
-    this.props.onClick(event)
-  }
-
-  render() {
-    const { className, kind, size, icon, label, ...rest } = this.props
-    const { shouldRenderConfirmation } = this.state
-
-    const componentClassName = classNames(
-      'c-CopyButton',
-      shouldRenderConfirmation && 'is-copyConfirmed',
-      icon && 'is-with-icon',
-      className
-    )
-    const iconSize = size === 'sm' ? '20' : '24'
-
-    return (
-      <CopyButtonUI
-        {...getValidProps(rest)}
-        kind={kind}
-        onClick={this.handleOnClick}
-        size={size}
-        className={componentClassName}
-      >
-        <ConfirmationIconWrapperUI>
-          <Icon
-            className="c-CopyButton__iconConfirmation"
-            name="checkmark"
-            size={iconSize}
-          />
-        </ConfirmationIconWrapperUI>
-        {icon && <IconUI size={iconSize} name={icon} />}
-        {label && <TextUI>{label}</TextUI>}
-      </CopyButtonUI>
-    )
-  }
+  return (
+    <CopyButtonUI
+      {...getValidProps(rest)}
+      onClick={handleClick}
+      size={size}
+      className={componentClassName}
+    >
+      <ConfirmationIconWrapperUI>
+        <Icon
+          className="c-CopyButton__iconConfirmation"
+          name="checkmark"
+          size={iconSize}
+        />
+      </ConfirmationIconWrapperUI>
+      {icon && <IconUI size={iconSize} name={icon} />}
+      {label && <TextUI>{label}</TextUI>}
+    </CopyButtonUI>
+  )
 }
 
 CopyButton.defaultProps = {
-  canRenderFocus: false,
   'data-cy': 'CopyButton',
   kind: 'secondary',
   label: 'Copy',
@@ -105,7 +80,6 @@ CopyButton.defaultProps = {
 }
 
 CopyButton.propTypes = {
-  canRenderFocus: PropTypes.bool,
   /** Custom class names to be added to the component. */
   className: PropTypes.string,
   /** Data attr for Cypress tests. */
