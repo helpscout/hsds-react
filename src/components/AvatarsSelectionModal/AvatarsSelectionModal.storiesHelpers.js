aadd3f88d4a02e3cea86b0cfec2c1a2117f25478
Jakub Janczyk <jakub@helpscout.com>
2021-11-17 12:45:51 +0100

    fixed stories

--- src/components/AvatarsSelectionModal/AvatarsSelectionModal.storiesHelpers.js
+++ src/components/AvatarsSelectionModal/AvatarsSelectionModal.storiesHelpers.js
@@ -1,4 +1,8 @@
+import React, { useState } from 'react'
 import { createSpec, derived, faker } from '@helpscout/helix'
+import AvatarsSelectionModal from './AvatarsSelectionModal'
+import { number } from '@storybook/addon-knobs'
+import Button from '../Button'
 
 const roles = ['administrator', 'account owner', 'user']
 
@@ -14,4 +18,62 @@ const avatarUserSpec = createSpec({
   role: faker.helpers.randomize(roles),
 })
 
-export const exampleAvatarsUsers = avatarUserSpec.generate(20)
+const exampleAvatarsUsers = avatarUserSpec.generate(20)
+
+export function DefaultAvatarsSelectionModal() {
+  const [opened, setOpened] = useState(false)
+  return (
+    <>
+      <Button onClick={() => setOpened(true)} kind="primary">
+        Open modal
+      </Button>
+      <AvatarsSelectionModal
+        isOpen={opened}
+        availableUsers={exampleAvatarsUsers}
+        selectionLimit={number('selectionLimit', 5)}
+        onClose={() => setOpened(false)}
+        onConfirm={() => setOpened(false)}
+      />
+    </>
+  )
+}
+
+export function SingleSelectionAvatarsSelectionModal() {
+  const [opened, setOpened] = useState(false)
+  return (
+    <>
+      <Button onClick={() => setOpened(true)} kind="primary">
+        Open modal
+      </Button>
+      <AvatarsSelectionModal
+        isOpen={opened}
+        availableUsers={exampleAvatarsUsers}
+        selectionLimit={1}
+        onClose={() => setOpened(false)}
+        onConfirm={() => setOpened(false)}
+      />
+    </>
+  )
+}
+
+export function PreSelectedAvatarsSelectionModal() {
+  const [opened, setOpened] = useState(false)
+  return (
+    <>
+      <Button onClick={() => setOpened(true)} kind="primary">
+        Open modal
+      </Button>
+      <AvatarsSelectionModal
+        isOpen={opened}
+        availableUsers={exampleAvatarsUsers}
+        selectionLimit={5}
+        initiallySelectedUsers={[
+          exampleAvatarsUsers[0].id,
+          exampleAvatarsUsers[5].id,
+        ]}
+        onClose={() => setOpened(false)}
+        onConfirm={() => setOpened(false)}
+      />
+    </>
+  )
+}
