7297d83cd901643d09bf82c00220f7ce8b1ed8bf
Pierre-Luc Babin <plbabin@gmail.com>
2021-12-06 18:54:10 +0000

    Create CopyValue component

---
+++ src/components/CopyValue/CopyValue.test.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { render, act } from '@testing-library/react'
+import CopyValue from './CopyValue'
+import userEvent from '@testing-library/user-event'
+
+jest.useFakeTimers()
+
+beforeAll(() => {
+  Object.assign(navigator, {
+    clipboard: {
+      writeText: () => {},
+    },
+  })
+})
+
+describe('ClassName', () => {
+  test('Has default component className', () => {
+    const { getByTestId } = render(<CopyValue value="test" />)
+
+    expect(getByTestId('CopyValue')).toHaveClass('c-CopyValue')
+  })
+
+  test('Applies custom className if specified', () => {
+    const className = 'gator'
+    const { getByTestId } = render(
+      <CopyValue className={className} value="test" />
+    )
+
+    expect(getByTestId('CopyValue')).toHaveClass(className)
+  })
+})
+
+describe('Timeout', () => {
+  test('Clears timeout on unmount', () => {
+    const spy = jest.spyOn(window, 'clearTimeout')
+    const { getByTestId, unmount } = render(<CopyValue value="test" />)
+    act(() => {
+      userEvent.click(getByTestId('IconButton'))
+      unmount()
+    })
+    expect(spy).toHaveBeenCalled()
+    spy.mockRestore()
+  })
+
+  test('Clears timeout on the second click', () => {
+    const spy = jest.spyOn(window, 'clearTimeout')
+    const { getByTestId } = render(<CopyValue value="test" />)
+    act(() => {
+      userEvent.click(getByTestId('IconButton'))
+      userEvent.click(getByTestId('IconButton'))
+    })
+
+    expect(spy).toHaveBeenCalled()
+
+    spy.mockRestore()
+  })
+
+  test('Renders, then resets confirmation UI on click', () => {
+    const { getByTestId } = render(<CopyValue value="test" />)
+    act(() => {
+      userEvent.click(getByTestId('IconButton'))
+    })
+    expect(getByTestId('IconButton')).toHaveClass('is-copyConfirmed')
+
+    act(() => {
+      jest.runAllTimers()
+    })
+    expect(getByTestId('IconButton')).not.toHaveClass('is-copyConfirmed')
+  })
+
+  test('Fires onReset callback when timeout completes', () => {
+    const spy = jest.fn()
+    const { getByTestId } = render(<CopyValue value="test" onReset={spy} />)
+    act(() => {
+      userEvent.click(getByTestId('IconButton'))
+
+      jest.runAllTimers()
+    })
+    expect(spy).toHaveBeenCalled()
+  })
+})
+
+describe('clipboard', () => {
+  test('Clicking the button will copy the value to the clipboard', () => {
+    const spy = jest.spyOn(window.navigator.clipboard, 'writeText')
+    const { getByTestId } = render(<CopyValue value="test" />)
+    act(() => {
+      userEvent.click(getByTestId('IconButton'))
+    })
+    expect(spy).toHaveBeenCalledWith('test')
+    spy.mockRestore()
+  })
+})
