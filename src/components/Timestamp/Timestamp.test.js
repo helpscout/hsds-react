58c198bc1a8ba0e8cba7330fd7efde7aadd63247
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2022-02-21 12:09:30 +0100

    Incorporates timestamp.js into Timestamp component

--- src/components/Timestamp/Timestamp.test.js
+++ src/components/Timestamp/Timestamp.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Timestamp from './Timestamp'
+import { calculateTimeoutPeriod } from './Timestamp.utils'
+
 import { Icon, Text } from '..'
 
 const cx = 'c-Timestamp'
@@ -151,5 +153,35 @@ describe('Styles', () => {
     const wrapper = mount(<Timestamp muted />)
 
     expect(wrapper.getDOMNode().classList.contains('is-muted')).toBeTruthy()
+  })
+})
+
+describe('calculateTimeoutPeriod', () => {
+  test('returns 0 when difference is more than a day', () => {
+    const timestamp = new Date()
+    timestamp.setTime(Date.now() - 60 * 60 * 24 * 3 * 1000)
+
+    expect(calculateTimeoutPeriod(timestamp)).toEqual(0)
+  })
+
+  test('returns an hour (in ms) when the difference is more than an hour', () => {
+    const timestamp = new Date()
+    timestamp.setTime(Date.now() - 60 * 60 * 3 * 1000)
+
+    expect(calculateTimeoutPeriod(timestamp)).toEqual(60 * 60 * 1000)
+  })
+
+  test('returns 15 seconds (in ms) when the difference is more than an minute', () => {
+    const timestamp = new Date()
+    timestamp.setTime(Date.now() - 60 * 3 * 1000)
+
+    expect(calculateTimeoutPeriod(timestamp)).toEqual(15 * 1000)
+  })
+
+  test('returns a second (in ms) when the difference is less than an minute', () => {
+    const timestamp = new Date()
+    timestamp.setTime(Date.now() - 45 * 1000)
+
+    expect(calculateTimeoutPeriod(timestamp)).toEqual(1000)
   })
 })
