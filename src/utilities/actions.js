1fbf4667bcbfaa295dd0ba98ced06f68b643685b
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2022-02-22 12:18:45 +0100

    Removes actions.js, moves tests to dropdown

--- src/utilities/actions.js
+++
@@ -1,5 +0,0 @@
-export const createActionTypes = (actionTypes = [], namespace = '@@HSDS') => {
-  return actionTypes.reduce((types, type) => {
-    return { ...types, [type]: `${namespace}/${type}` }
-  }, {})
-}
