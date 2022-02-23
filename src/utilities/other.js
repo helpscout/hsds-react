29d36fff383ac6e1ca629c4fbecff6e8298ef68b
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2022-02-23 15:02:31 +0100

    Process other.js

--- src/utilities/other.js
+++ src/utilities/other.js
@@ -1,14 +1,4 @@
 export function noop() {}
-export const promiseNoop = () => Promise.resolve()
-
-export const requestAnimationFrame = callback => {
-  // This method was mostly created for node testing.
-  // JSDOM (used by Enzyme) doesn't support requestAnimationFrame.
-
-  return window.requestAnimationFrame
-    ? window.requestAnimationFrame(callback)
-    : callback()
-}
 
 export const isNodeEnv = () => {
   const process = window.process
