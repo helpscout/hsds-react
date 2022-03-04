f1601af6eccb33d18a1a73da82690be91ab6aa12
Juan Pablo Lomeli Diaz <juanpablo@thetinkertrain.codes>
2022-03-04 11:51:31 +0100

    Adds SimpleModal nested story

--- src/components/SimpleModal/SimpleModal.storiesHelpers.js
+++ src/components/SimpleModal/SimpleModal.storiesHelpers.js
@@ -1,24 +1,69 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import SimpleModal from './SimpleModal'
+import { Portal } from '../../hooks/usePortal'
 import Button from '../Button'
 
-export const SimpleModalWithTrigger = () => {
-  const [isOpened, setIsOpened] = useState(false)
+function reducer(state, type) {
+  switch (type) {
+    case 'OPEN_1':
+      return { ...state, isOpen1: true }
+    case 'CLOSE_1':
+      return { ...state, isOpen1: state.isOpen2 ? true : false }
+    case 'OPEN_2':
+      return { ...state, isOpen2: true }
+    case 'CLOSE_2':
+      return { ...state, isOpen2: false }
 
+    default:
+      return state
+  }
+}
+
+export const SimpleModalWithTrigger = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    isOpen1: false,
+    isOpen2: false,
+  })
   return (
     <>
-      <Button kind="primary" onClick={() => setIsOpened(true)}>
+      <Button kind="primary" onClick={() => dispatch('OPEN_1')}>
         Open modal
       </Button>
-      <SimpleModal
-        show={isOpened}
-        focusModalOnShow
-        trapFocus
-        onClose={() => setIsOpened(false)}
-      >
-        <div>My modal</div>
-        <Button kind="primary">Some button</Button>
-      </SimpleModal>
+      <Portal selector="#root">
+        <SimpleModal
+          show={state.isOpen1}
+          className="first-modal"
+          focusModalOnShow
+          trapFocus
+          onClose={e => {
+            dispatch('CLOSE_1')
+          }}
+          closeOnClickOutside="modal"
+        >
+          <div>My modal</div>
+          <Button kind="primary" onClick={() => dispatch('OPEN_2')}>
+            Open second modal
+          </Button>
+        </SimpleModal>
+      </Portal>
+      <Portal selector="#root">
+        <SimpleModal
+          id="second-modal"
+          className="second-modal"
+          show={state.isOpen2}
+          height="150px"
+          width="150px"
+          focusModalOnShow
+          trapFocus
+          onClose={e => {
+            dispatch('CLOSE_2')
+            document.querySelector('#simple-modal').focus()
+          }}
+          closeOnClickOutside="modal"
+        >
+          <div>My second modal</div>
+        </SimpleModal>
+      </Portal>
     </>
   )
 }
