27d8cd3b98fa1585a0f64edc8102e5e21230fec3
Jakub Janczyk <jakubjanczyk@users.noreply.github.com>
2022-04-29 10:50:02 +0200

    HSDS-259: Embedding video in Message and Attachment custom content (#1055)
    * HSDS-259: Allow embedding video in a Message
    
    * add play video large icon
    
    * allow to provide custom content to Attachment
    
    * fixed test name
    
    * apply review comments
    
    * add border radius to video iframe
    
    * set title for video iframe if it's missing

---
+++ src/components/MessageCard/components/MessageCard.Video.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const MessageCardVideoContainerUI = styled.div`
+  iframe {
+    border-radius: 3px;
+  }
+`
+
+export const MessageCardVideo = ({ video, onLoad }) => {
+  const videoIframeRef = useRef(null)
+  const videoContainerRef = useRef(null)
+
+  useEffect(() => {
+    if (video && videoContainerRef.current) {
+      videoIframeRef.current = videoContainerRef.current.querySelector('iframe')
+      if (videoIframeRef.current) {
+        videoIframeRef.current.onload = () => onLoad()
+        videoIframeRef.current.onerror = () => onLoad()
+        const iframeTitle = videoIframeRef.current.getAttribute('title')
+        if (!iframeTitle) {
+          videoIframeRef.current.setAttribute('title', 'Message video')
+        }
+      } else {
+        // in case something went wrong rendering iframe, just show a Message
+        onLoad()
+      }
+    }
+
+    return () => {
+      if (videoIframeRef.current) {
+        videoIframeRef.current.onload = undefined
+        videoIframeRef.current.onerror = undefined
+      }
+    }
+  }, [onLoad, video])
+
+  return (
+    <MessageCardVideoContainerUI
+      ref={videoContainerRef}
+      dangerouslySetInnerHTML={{ __html: video.html }}
+    />
+  )
+}
+
+MessageCardVideo.propTypes = {
+  /** Video to render */
+  video: PropTypes.shape({
+    html: PropTypes.string.isRequired,
+  }).isRequired,
+  /** Callback when video loaded */
+  onLoad: PropTypes.func,
+}
+
+MessageCardVideo.defaultProps = {
+  onLoad: () => {},
+}
